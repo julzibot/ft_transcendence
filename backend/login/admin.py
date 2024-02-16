cc2f005580acaef199d74ca3bb5fe2dd64d9bd6b
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-02-16 20:16:48 +0100

    added email to Login model

--- backend/login/admin.py
+++ backend/login/admin.py
@@ -5,6 +5,6 @@ from .models import Login
 @admin.register(Login)
 class LoginAdmin(admin.ModelAdmin):
   
-  list_display = ('id', 'username', 'login_date', 'is_logged')
-  list_filter = ('login_date', 'is_logged')
+  list_display = ('id', 'username', 'email', 'login_date', 'is_logged')
+  list_filter = ('login_date', 'email', 'is_logged')
   search_fields = ('username', )
