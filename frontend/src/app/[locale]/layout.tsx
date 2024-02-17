a0b30d482cb4f71e71fb5d028d193c526fff7b6a
Tosh-Sharma <toshkumar.s@somaiya.edu>
2024-02-17 11:36:01 +0100

    Updated code to have translations into the picture.

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css"
+import { NextIntlClientProvider, useMessages } from 'next-intl';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +12,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params: { locale}
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string};
 }>) {
+  const messages = useMessages();
+ 
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang={locale}>
+      <body className={inter.className}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
     </html>
   );
 }
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,3 +1,53 @@
-export {default} from "next-auth/middleware"
+import { NextRequest } from 'next/server';
+import createMiddleware from 'next-intl/middleware';
+import { withAuth } from 'next-auth/middleware';
+import  { locales } from './i18n';
 
-export const config = { matcher: ["/signup"]}
+export const config = {
+  // Match only internationalized pathnames
+  matcher: ['/', '/(en|fr|jp)/:path*']
+};
+
+const publicPages = ['/login'];
+
+const intlMiddleware = createMiddleware({
+  // A list of all locales that are supported
+  locales,
+ 
+  // Used when no locale matches
+  defaultLocale: 'en'
+});
+
+
+const authMiddleware = withAuth(
+  // Note that this callback is only invoked if
+  // the `authorized` callback has returned `true`
+  // and not for pages listed in `pages`.
+  function onSuccess(req) {
+    return intlMiddleware(req);
+  },
+  {
+    callbacks: {
+      authorized: ({token}) => token != null
+    },
+    pages: {
+      signIn: '/login'
+    }
+  }
+);
+ 
+export default function middleware(req: NextRequest) {
+  const publicPathnameRegex = RegExp(
+    `^(/(${locales.join('|')}))?(${publicPages
+      .flatMap((p) => (p === '/' ? ['', '/'] : p))
+      .join('|')})/?$`,
+    'i'
+  );
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
+ 
+  if (isPublicPage) {
+    return intlMiddleware(req);
+  } else {
+    return (authMiddleware as any)(req);
+  }
+}
