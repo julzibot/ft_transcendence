e1bea4f7929e645caf849d36ccd086233144a898
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-02-19 15:50:16 +0100

    created user auth apis and token/cookie management

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('api/', include('users.urls'))
 ]
