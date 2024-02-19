4d441a911380326c8bca33ccd460a5e3e7aa32c4
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-02-19 13:01:11 +0100

    created users

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'login',
+    'rest_framework',
+    'djoser',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+DJOSER = {
+  
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.UserAccount"
