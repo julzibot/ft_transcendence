4dba9f70823ddd38efafb7937c7ab652aa53665d
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-02-21 18:11:51 +0100

    nearly there ?

--- frontend/src/app/[locale]/components/AuthButton.tsx
+++ frontend/src/app/[locale]/components/AuthButton.tsx
@@ -4,13 +4,14 @@ import { signIn, signOut, useSession} from "next-auth/react"
 import Link from "next/link";
 
 export default function AuthButton() {
-  const { data:session} = useSession();
+  const { data: session} = useSession();
+  console.log(session?.backendTokens)
 
-  if(session) {
+  if(session && session.user) {
     return (
       <>
-        <h1 className="text-light"> Hello {session?.user?.name } </h1>
-        <Link href="/dashboard">Dashboard</Link>
+        <h1 className="text-light"> Hello {session?.user.name} </h1>
+        <Link href="/en/dashboard">Dashboard</Link>
         <button className="btn btn-primary" onClick={() => signOut()}>
           Sign Out
         </button>
