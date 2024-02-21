e74aba4e1adda39f61cb69a0327b16b8b1032821
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-02-21 19:45:50 +0100

    preparing to do a profile page

--- frontend/src/app/[locale]/components/AuthButton.tsx
+++ frontend/src/app/[locale]/components/AuthButton.tsx
@@ -5,12 +5,11 @@ import Link from "next/link";
 
 export default function AuthButton() {
   const { data: session} = useSession();
-  console.log(session?.backendTokens)
 
   if(session && session.user) {
     return (
       <>
-        <h1 className="text-light"> Hello {session?.user.name} </h1>
+        <h1 className="text-light"> Hello {session.user.name} </h1>
         <Link href="/en/dashboard">Dashboard</Link>
         <button className="btn btn-primary" onClick={() => signOut()}>
           Sign Out
