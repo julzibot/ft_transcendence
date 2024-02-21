3f84ef761b1fc613b31b7ae3a27b051bc46a00d3
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-02-21 12:35:38 +0100

    42auth now save user in database and fetch user when already registerd

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css"
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+// import { NextIntlClientProvider, useMessages } from 'next-intl';
+import SessionProvider from "./components/SessionProvider";
+import { getServerSession } from "next-auth";
+import AppNav from "./components/AppNav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,21 +13,25 @@ export const metadata: Metadata = {
   description: "An online pong game",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
   params: { locale}
 }: Readonly<{
   children: React.ReactNode;
   params: { locale: string};
 }>) {
-  const messages = useMessages();
+  // const messages = useMessages();
+  const session = await getServerSession();
  
   return (
     <html lang={locale}>
       <body className={inter.className}>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          {children}
-        </NextIntlClientProvider>
+        {/* <NextIntlClientProvider locale={locale} messages={messages}> */}
+          <SessionProvider session={session}>
+            <AppNav />
+            {children}
+          </SessionProvider>
+        {/* </NextIntlClientProvider> */}
       </body>
     </html>
   );
--- frontend/src/app/api/auth/[...nextauth]/options.ts
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -1,12 +1,43 @@
 import type {NextAuthOptions} from 'next-auth'
+import FortyTwo from 'next-auth/providers/42-school';
 import FortyTwoProvider from "next-auth/providers/42-school";
+import Email from 'next-auth/providers/email';
+import { notFound } from 'next/navigation';
 //import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const options: NextAuthOptions = {
   providers: [
     FortyTwoProvider({
       clientId: process.env.FORTY_TWO_CLIENT_ID as string,
-      clientSecret: process.env.FORTY_TWO_CLIENT_SECRET as string
-    })
-  ]
+      clientSecret: process.env.FORTY_TWO_CLIENT_SECRET as string,
+    }),
+  ],
+
+  callbacks: {
+    async signIn({user}) {
+      const userName = user.name
+      const userEmail = user.email
+      let response = await fetch('http://localhost:8000/api/login/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: userName, email: userEmail}),
+      });
+
+      if(response.status === 401) {
+        response = await fetch('http://localhost:8000/api/register/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name: userName, email: userEmail}),
+        });
+      }
+      if(response.ok)
+        return true;
+      else
+        return false;
+    }
+  }
 }
