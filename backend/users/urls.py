f29cbefa5b197f4cdf6ccf0fbfe594e28c2065f2
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-02-22 20:55:21 +0100

    Setting refresh tokens

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserView, LogoutView
+from .views import LoginView, UserView, RefreshView
 
 urlpatterns = [
-  path('register/', RegisterView.as_view()),
   path('login/', LoginView.as_view()),
   path('user/', UserView.as_view()),
-  path('logout/', LogoutView.as_view())
+  path('refresh/', RefreshView.as_view()),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,64 +1,58 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework import status
 from .models import UserAccount
-from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
-import datetime, jwt
+from rest_framework_simplejwt.tokens import RefreshToken
+import jwt
 from .serializers import UserAccoutSerializer
-from django.conf import settings
+import os
+from dotenv import load_dotenv
 
-# Register New User in Database
-class RegisterView(APIView):
-  def post(self, request):
-    serializer = UserAccoutSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data)
+load_dotenv()
 
-# Fecths User in Database and returns its tokens
+def get_tokens_for_user(user):
+  refresh = RefreshToken.for_user(user)
+
+  return{
+    'refresh': str(refresh),
+    'access': str(refresh.access_token),
+  }
+
+def registerUser(data):
+  serializer = UserAccoutSerializer(data=data)
+  serializer.is_valid(raise_exception=True)
+  serializer.save()
+  return Response(serializer.data)
+
+
 class LoginView(APIView):
   def post(self, request):
-    email = request.data['email']
-    name = request.data['name']
-    # password = request.data['password']
-
+    data = request.data
+    name = data['name']
+    email = data['email']
     user = UserAccount.objects.filter(email=email).first()
-
-    if user is None:
-      raise AuthenticationFailed('User not found.')
     
-    # if not user.check_password(password):
-    #   raise AuthenticationFailed('Incorrect Password')
-
-    access_token = AccessToken.for_user(user)
-    refresh_token = RefreshToken.for_user(user)
-
-    response = Response()
-    # response.set_cookie(key='jwt-access', value = access_token, httponly=True)
-    # response.set_cookie(key='jwt-refresh', value = refresh_token, httponly=True)
-    response.data = {
+    if not user:
+      user = registerUser(data)
+    return Response({
       'user': {
         'id': user.id,
-        'name': str(name),
-        'email': str(email)
+        'name': name,
+        'email': email
       },
-      'backendTokens': {
-      'accessToken': str(access_token),
-      'refreshToken': str(refresh_token)
-      }
-    }
-    return response
+      'backendTokens': get_tokens_for_user(user)})
   
 class UserView(APIView):
   def get(self, request):
     access_token = request.META.get('HTTP_AUTHORIZATION')
     access_token = access_token.split(' ')[1]
-    # refresh_token = request.COOKIES.get('jwt-refresh')
+
 
     if not access_token:
       raise AuthenticationFailed('Unauthenticated')
     try:
-      payload = jwt.decode(access_token, settings.SECRET_KEY, algorithms=['HS256'])
+      payload = jwt.decode(access_token, os.getenv('TOKEN_SIGNING_KEY'), algorithms=['HS256'])
     except jwt.ExpiredSignatureError:
       raise AuthenticationFailed('Unauthenticated')
     
@@ -67,13 +61,27 @@ class UserView(APIView):
   
     return Response(serializer.data)
 
-class LogoutView(APIView):
+class RefreshView(APIView):
   def post(self, request):
-    response = Response()
-    response.delete_cookie('jwt-access')
-    response.delete_cookie('jwt-refresh')
-    response.data = {
-      'message': 'success'
-    }
-    return response
+    refresh_token = request.META.get('HTTP_AUTHORIZATION')
+    if not refresh_token:
+      return Response({'error': 'Authorization: refresh is required'}, status=status.HTTP_400_BAD_REQUEST)
+    refresh_token = refresh_token.split(' ')
+    if refresh_token[0] != 'Refresh':
+      return Response({'error': 'Authorization: refresh is required'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    refresh_token = refresh_token[1]
+    if not refresh_token:
+      return Response({'error': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        refresh = RefreshToken(refresh_token)
+        access_token = str(refresh.access_token)
+        return Response(
+          {
+            'refresh': str(refresh),
+            'access': access_token
+          })
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
   
