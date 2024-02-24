0b5df73b99667aca56254ec1fad6ef3ab1065da5
Julzibot <ibotjules@gmail.com>
2024-02-24 16:17:18 +0100

    still git stuff

---
+++ frontend/src/app/[locale]/lib/Constants.ts
@@ -0,0 +1,13 @@
+// import * as THREE from 'three';
+
+export const GAMEWIDTH = 45;
+export const GAMEHEIGHT = 30;
+export const PLAYERLEN = 4.5;
+export const BALLRADIUS = 0.6;
+export const SPEEDINCREMENT = 1.07;
+export const BALLMAXSPEED = 0.8;
+export const MINREBOUNDANGLE = 40;
+export const PLAYERSPEED = 0.65
+export const WINSCORE = 3;
+export const FONTPATH = "fonts/";
+export const FONTNAME = "Norwester_Regular.json";
