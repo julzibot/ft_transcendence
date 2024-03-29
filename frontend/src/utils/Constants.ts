0fc7223d971adc7dd6597f34e857df53c155dd95
Julzibot <ibotjules@gmail.com>
2024-03-29 17:39:33 +0100

    game re-functional

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -1,13 +1,35 @@
-// import * as THREE from 'three';
+import * as THREE from 'three';
+
+// const vars = {};
+// const csts = {};
+
+// vars.scoreMsg = new THREE.Mesh();
+// vars.ballVect = new THREE.Vector2(-1, 0);
+// vars.p1textMesh = new THREE.Mesh();
+// vars.p2textMesh = new THREE.Mesh();
+// vars.p1Score = 0;
+// vars.p2Score = 0;
+// vars.scoreString = "";
+// vars.endString = "";
+// vars.ballSpeed = 0.35;
+// vars.adjustedBallSpeed = 0.35;
+// vars.dotProduct = 0;
+// vars.stopGame = false;
+// vars.directions = [1,1];
+// vars.reboundDiff = 0;
+// vars.isRebound = 0;
+
+
 
 export const GAMEWIDTH = 45;
 export const GAMEHEIGHT = 30;
-export const PLAYERLEN = 4.5;
+export const PLAYERLEN = 5;
 export const BALLRADIUS = 0.6;
 export const SPEEDINCREMENT = 1.07;
-export const BALLMAXSPEED = 0.8;
+export const BALLMAXSPEED = 0.5;
+export const BASE_BALLSPEED = 0.25;
 export const MINREBOUNDANGLE = 40;
-export const PLAYERSPEED = 0.65
+export const PLAYERSPEED = 0.5
 export const WINSCORE = 3;
 export const FONTPATH = "fonts/";
 export const FONTNAME = "Norwester_Regular.json";
