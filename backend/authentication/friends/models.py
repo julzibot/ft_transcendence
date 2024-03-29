408b5cbce3b7ada5cd9b6a39c9d2b33ac31aab65
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-03-29 12:39:25 +0100

    Friend request

--- backend/authentication/friends/models.py
+++ backend/authentication/friends/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from users.models import UserAccount
 
-class FriendList(models.Model):
-    user_id1 = models.ForeignKey(UserAccount, related_name='user_id1_friendlists', on_delete=models.CASCADE)
-    user_id2 = models.ForeignKey(UserAccount, related_name='user_id2_friendlists', on_delete=models.CASCADE)
-
-class FriendRequest(models.Model):
+class Friendship(models.Model):
     class REQUESTOR_CHOICES(models.TextChoices):
       UID1 = 'UID1',
       UID2 = 'UID2'
 
+    class STATUS_CHOICES(models.TextChoices):
+      FRIENDS = 'FRIENDS',
+      REQUEST = "REQUEST"
     user_id1 = models.ForeignKey(UserAccount, related_name='user_id1_friendrequests', on_delete=models.CASCADE)
     user_id2 = models.ForeignKey(UserAccount, related_name='user_id2_friendrequests', on_delete=models.CASCADE)
     requestor = models.CharField(max_length=4, choices=REQUESTOR_CHOICES)
+    status = models.CharField(max_length=7, choices=STATUS_CHOICES)
+    request_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
