7e517769e4319020505cd51e4aa4761439fa3bbd
Julzibot <ibotjules@gmail.com>
2024-04-10 14:06:46 +0200

    better player move + frame render

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -24,10 +24,10 @@ import * as THREE from 'three';
 export const GAMEWIDTH = 45;
 export const GAMEHEIGHT = 30;
 export const PLAYERLEN = 5;
-export const BASE_PLAYERSPEED = 0.21
-export const PLAYERSPEED_INCREMENT = 1.035
-export const PLAYERSPEED_MAX = 0.42
-export const BALLRADIUS = 0.7;
+export const BASE_PLAYERSPEED = 0.17
+export const PLAYERSPEED_INCREMENT = 1.023
+export const PLAYERSPEED_MAX = 0.45
+export const BALLRADIUS = 0.6;
 export const BASE_BALLSPEED = 0.15;
 export const BALLSPEED_INCREMENT = 1.07;
 export const BALLSPEED_MAX = 0.38;
