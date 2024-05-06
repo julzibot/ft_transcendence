74dde4fc43e868ccf0059d76e3752040e1a15508
Julzibot <ibotjules@gmail.com>
2024-05-06 11:56:06 +0200

    ball lighting + color

--- frontend/src/utils/algo.jsx
+++ frontend/src/utils/algo.jsx
@@ -9,16 +9,19 @@ export const vars = {};
 objs.sphereGeo = new THREE.SphereGeometry(CONST.BALLRADIUS, 40, 40);
 objs.playerGeo = new THREE.BoxGeometry(0.5, CONST.PLAYERLEN, 0.6);
 objs.upDownBoundary = new THREE.BoxGeometry(CONST.GAMEWIDTH, 0.5, 0.6);
+objs.BackgroundGeo = new THREE.SphereGeometry(CONST.DECORSIZE, 40, 40);
 
-objs.sphereMaterial = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
-objs.boundaryMaterial = new THREE.MeshStandardMaterial( { color: 0x0000ff } );
+objs.sphereMaterial = new THREE.MeshPhongMaterial( { color: CONST.BALL_COLOR, emissive: CONST.BALL_COLOR, emissiveIntensity: 0.1 } );
+objs.boundaryMaterial = new THREE.MeshStandardMaterial( { color: 0x0000ff, opacity: 0.5, transparent: true } );
 objs.playerMaterial = new THREE.MeshStandardMaterial( { color: 0xff00ff } );
+objs.BackgroundMaterial = new THREE.MeshPhongMaterial( { color: 0x8f9fcc, side: THREE.BackSide } );
 
 objs.ball = new THREE.Mesh( objs.sphereGeo, objs.sphereMaterial );
 objs.player1 = new THREE.Mesh( objs.playerGeo, objs.playerMaterial );
 objs.player2 = new THREE.Mesh( objs.playerGeo, objs.playerMaterial );
 objs.topB = new THREE.Mesh( objs.upDownBoundary, objs.boundaryMaterial );
 objs.botB = new THREE.Mesh( objs.upDownBoundary, objs.boundaryMaterial );
+objs.background = new THREE.Mesh( objs.BackgroundGeo, objs.BackgroundMaterial );
 
 objs.player1.position.set(-CONST.GAMEWIDTH / 2, 0, 0);
 objs.player2.position.set(CONST.GAMEWIDTH / 2, 0, 0);
@@ -31,7 +34,8 @@ csts.botHB = new THREE.Box3().setFromObject(objs.botB);
 csts.gameVect = new THREE.Vector2(objs.player2.position.x - objs.player1.position.x, objs.player2.position.y - objs.player1.position.y).normalize();
 csts.loader = new FontLoader();
 csts.ambLight = new THREE.AmbientLight(0x444444);
-csts.dirLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+csts.dirLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
+csts.ballLight = new THREE.PointLight( 0xff0000, 1, 30);
 csts.dirLight.position.set(5, 7, 15);
 csts.game_id = 0;
 
@@ -44,6 +48,8 @@ vars.p1Score = 0;
 vars.p2Score = 0;
 vars.scoreString = "";
 vars.endString = "";
+vars.glowStartTime = 0;
+vars.glowElapsed = 0;
 
 vars.ballVect = new THREE.Vector2(-1, 0);
 vars.ballSpeed = CONST.BASE_BALLSPEED;
--- frontend/src/utils/constants.jsx
+++ frontend/src/utils/constants.jsx
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 
 export const GAMEWIDTH = 45;
 export const GAMEHEIGHT = 30;
+export const DECORSIZE = 100;
 
 export const PLAYERLEN = 5;
 export const BASE_PLAYERSPEED = 0.17
@@ -12,6 +13,7 @@ export const BALLRADIUS = 0.6;
 export const BASE_BALLSPEED = 0.15;
 export const BALLSPEED_INCREMENT = 1.07;
 export const BALLSPEED_MAX = 0.38;
+export const BALL_COLOR = 0x0000ff;
 
 export const MINREBOUNDANGLE = 40;
 export const WINSCORE = 2;
