4b1199eb2fb67cb05f2062e38a7239376a9605b8
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-05-07 11:44:03 +0200

    sign in with credentials works

--- backend/authentication/users/urls.py
+++ backend/authentication/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from dashboard.views import DashboardView
-from .views import SigninView, UserView, UpdateNameView, SignupView, CustomTokenRefreshView, SearchUserView
+from .views import SigninView, UserView, UpdateNameView, SignupView, CustomTokenRefreshView, SearchUserView, AccessTokenView
 
 urlpatterns = [
-  path('signin/', SigninView.as_view()),
+  path('auth/signin/', SigninView.as_view()),
   path('signup/', SignupView.as_view()),
-  path('user/', UserView.as_view()),
+  path('auth/access_token/', AccessTokenView.as_view()),
+  path('auth/user/', UserView.as_view()),
   path('refresh/', CustomTokenRefreshView.as_view()),
   path('update/name/', UpdateNameView.as_view()),
   path('search-user/', SearchUserView.as_view()),
