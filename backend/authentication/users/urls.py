e7f0731907981bb21824b1c50fd021e9a54a6ab0
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-05-08 12:37:02 +0200

    register and login with credentials works

--- backend/authentication/users/urls.py
+++ backend/authentication/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from dashboard.views import DashboardView
-from .views import SigninView, UserView, UpdateNameView, SignupView, CustomTokenRefreshView, SearchUserView, AccessTokenView
+from .views import SigninView, UserView, UpdateNameView, RegisterView, CustomTokenRefreshView, SearchUserView, AccessTokenView
 
 urlpatterns = [
   path('auth/signin/', SigninView.as_view()),
-  path('signup/', SignupView.as_view()),
+  path('auth/register/', RegisterView.as_view()),
   path('auth/access_token/', AccessTokenView.as_view()),
   path('auth/user/', UserView.as_view()),
   path('refresh/', CustomTokenRefreshView.as_view()),
---
+++ frontend/src/app/(auth)/signin/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import "react"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation";
+import Image from "next/image"
+import Link from "next/link"
+import {useState} from "react"
+
+export default function SigninPage() {
+  const [data, setData] = useState({
+    login:'',
+    email:'',
+    password:''
+  });
+  const router = useRouter();
+
+  async function loginUser(e: React.FormEvent<HTMLInputElement>) {
+    e.preventDefault();
+    signIn('credentials', {
+      ...data,
+      redirect: false
+      }
+    )
+    router.push('/dashboard')
+  }
+  return (
+    <>  
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card shadow-lg text-center rounded-1 border-0">
+          <div className="card-header fs-2 fw-bold">Sign in to your account</div>
+          <div className="card-body">
+            <button className="btn btn-dark  fs-4 fw-bold"onClick={() => signIn('42-school')}>
+            Sign in with
+            <Image
+                className="ms-1 me-1"
+                src="/static/images/42.png"
+                style={{filter: "invert(100%)"}}
+                width={30}
+                height={30}
+                alt="42 Logo"
+              />
+            </button>
+            <p>Or</p>
+            <form onSubmit={loginUser}>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label" >Email address</label>
+                <input type="email" id="email" className ="form-control" value={data.email} onChange={(e) => setData({...data, email:e.target.value})}/>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" value={data.password} onChange={(e) => setData({...data, password:e.target.value})}/>
+              </div>
+              <button type="submit" className="btn btn-dark fw-bold">Submit</button>
+            </form>
+          </div>
+          <div className="card-footer">Not Registered Yet ? 
+            <Link className="text-decoration-none"href="/register"> Register</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
