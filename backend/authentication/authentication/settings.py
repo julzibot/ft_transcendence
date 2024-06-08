e669dbbb11676f8b4fa09450ff4e18cfc1c1093c
Astha Shah <astha.shah005@gmail.com>
2024-06-08 22:05:48 +0530

    Working on tournament apis

--- backend/authentication/authentication/settings.py
+++ backend/authentication/authentication/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'friends',
     'game',
     'chat',
+    'tournament',
 ]
 
 MIDDLEWARE = [
--- backend/authentication/tournament/views.py
+++ backend/authentication/tournament/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import TournamentData
+from .serializers import TournamentSerializer
 
 # Create your views here.
+class TournamentView(APIView):
+	def get(self, request):
+		tournament = TournamentData.objects.all()
+		serializer = TournamentSerializer(tournament, many=True)
+		return Response(serializer.data)
