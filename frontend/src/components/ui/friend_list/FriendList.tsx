6a80b1665528591b5d53affa888115ab82d53f3b
Antoineloisea1 <antoineloisea1@gmail.com>
2024-06-12 15:39:18 +0200

    Debounced User Search, added some toasts

--- frontend/src/components/ui/friend_list/FriendList.tsx
+++ frontend/src/components/ui/friend_list/FriendList.tsx
@@ -1,58 +1,112 @@
 "use client";
+
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import { Trash3Fill, Joystick, Dot, ChatDotsFill } from 'react-bootstrap-icons';
 import { CustomTooltip } from '@/components/Utils/Tooltip';
 import SearchPlayerInput from './SearchPlayerInput';
 
 export default function FriendList() {
-const friends = [{
-	name: 'John Doe',
-	isOnline: true,
-	}, {
-	name: 'Jane Doe',
-	isOnline: false,
-	}, {
-	name: 'Foo Bar',
-	isOnline: true,
-	}, {
-	name: 'Baz Qux',
-	isOnline: false,
-	}];
+	const friends = [{
+		name: 'John Doe',
+		isOnline: true,
+		}, {
+		name: 'Jane Doe',
+		isOnline: false,
+		}, {
+		name: 'Foo Bar',
+		isOnline: true,
+		}, {
+		name: 'Baz Qux',
+		isOnline: false,
+		}];
 
 	return (
-    <div className="border-left border-dark pl-3">
-	<SearchPlayerInput />
-	<div className="d-flex flex-column pt-4">
-		{friends.map((friend, index) => (
-			<div key={index} className="d-flex flex-row justify-content-between">
-				{/**TODO: RENDER IMAGES OF USERS */}
-				<div className="">
-					<img src={friend.avatar} />
-				</div>
-				<div className="">
-					<h3>{friend.name}</h3>
-					<p>{friend.status}</p>
-				</div>
-				<div className="">
-					<CustomTooltip text="Invite to play" position="top">
-						<button className='btn'>
-							<Joystick color="green" />
-						</button>
-					</CustomTooltip>
-					<CustomTooltip text="remove friend" position="top">
-						<button className='btn'>
-							<Trash3Fill color="red" />
-						</button>
-					</CustomTooltip>
-					<CustomTooltip text="Send message" position="top">
-						<button className='btn'>
-							<ChatDotsFill color="blue" />
-						</button>
-					</CustomTooltip>
+    <div>
+			<button 
+				class="btn btn-secondary btn-sm dropdown-toggle" 
+				type="button" 
+				data-bs-toggle="collapse" 
+				data-bs-target="#friendsCollapse" 
+				aria-expanded="false" 
+				aria-controls="friendsCollapse">
+				Friends
+			</button>
+			<div className="collapse" id="friendsCollapse">
+				<div className="card card-body">
+					{
+						friends.map((friend, index) => (
+							<div key={index} className="d-flex flex-row justify-content-between">
+								<div className="">
+									<img src={friend.avatar} />
+								</div>
+								<div className="">
+									<h3>{friend.name}</h3>
+									<p>{friend.status}</p>
+								</div>
+								<div className="">
+									<CustomTooltip text="Invite to play" position="top">
+										<button className='btn'>
+											<Joystick color="green" />
+										</button>
+									</CustomTooltip>
+									<CustomTooltip text="remove friend" position="top">
+										<button className='btn'>
+											<Trash3Fill color="red" />
+										</button>
+									</CustomTooltip>
+									<CustomTooltip text="Send message" position="top">
+										<button className='btn'>
+											<ChatDotsFill color="blue" />
+										</button>
+									</CustomTooltip>
+								</div>
+							</div>
+						))}
 				</div>
 			</div>
-		))}
-	</div>
+			<button 
+				class="btn btn-secondary btn-sm dropdown-toggle" 
+				type="button" 
+				data-bs-toggle="collapse" 
+				data-bs-target="#requestedFriendsCollapse" 
+				aria-expanded="false" 
+				aria-controls="collapseExample">
+				Friend Requests
+			</button>
+			<div className="collapse" id="requestedFriendsCollapse">
+				<div className="card card-body">
+					{
+							friends.map((friend, index) => (
+								<div key={index} className="d-flex flex-row justify-content-between">
+									<div className="">
+										<img src={friend.avatar} />
+									</div>
+									<div className="">
+										<h3>{friend.name}</h3>
+										<p>{friend.status}</p>
+									</div>
+									<div className="">
+										<CustomTooltip text="Invite to play" position="top">
+											<button className='btn'>
+												<Joystick color="green" />
+											</button>
+										</CustomTooltip>
+										<CustomTooltip text="remove friend" position="top">
+											<button className='btn'>
+												<Trash3Fill color="red" />
+											</button>
+										</CustomTooltip>
+										<CustomTooltip text="Send message" position="top">
+											<button className='btn'>
+												<ChatDotsFill color="blue" />
+											</button>
+										</CustomTooltip>
+									</div>
+								</div>
+							))}
+				</div>
+			</div>
+			<SearchPlayerInput />
     </div>
   )
 }
--- frontend/src/components/ui/friend_list/SearchPlayerInput.tsx
+++ frontend/src/components/ui/friend_list/SearchPlayerInput.tsx
@@ -1,30 +1,40 @@
 "use client";
 
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { PersonAdd  } from "react-bootstrap-icons";
 import { CustomTooltip } from "@/components/Utils/Tooltip";
 import { useSession } from "next-auth/react";
+import useDebounce from "@/components/Utils/CustomHooks/useDebounce";
+import { Col, Row, Toast, ToastContainer } from 'react-bootstrap'
 
 export default function SearchPlayerInput() {
   const [searchQuery, setSearchQuery] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState<string>('');
   const {data: session} = useSession()
+  const [toastParams, setToastParams] = useState({
+    text: '',
+    color: '',
+    title: '',
+    show: false
+  })
 
-  const handleSearch = async (event: any) => {
-    setInputValue(event.target.value)
-    const response = await fetch("http://localhost:8000/api/search-user/", {
-      method: "post",
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        id: session?.user.id,
-        query: event.target.value})
+  const debounce = useDebounce(inputValue, 800)
+
+  useEffect(() => {
+    fetchData()
+  }, [debounce])
+
+
+  const fetchData = async () => {
+    const response = await fetch(`http://localhost:8000/api/search-user/?query=${inputValue}&id=${session.user.id}`, {
+      method: "GET",
     })
     if(!response.ok) {
       setSearchQuery([])
       return false
     }
-    const res = await response.json()
-    setSearchQuery(res.users)
+    const data = await response.json()
+    setSearchQuery(data.users)
   }
 
   const handleFriendRequest = async(fromUserId: number, toUserId: number) => {
@@ -36,42 +46,72 @@ export default function SearchPlayerInput() {
         "to_user": toUserId
       })
     }).then(response => {
-      if (response.status === 201)
-        console.log('Friend request sent');
-      else
-        console.log('Friend request already sent');
+      if (response.status === 201) {
+        setToastParams({
+          text: 'Request successfully sent', 
+          color: 'success', 
+          title: 'Success', 
+          show: true,
+        })
+      }
+      else {
+        setToastParams({text: 'You Already sent a request to that person, wait for the reply.', color: 'danger', title: 'Warning', show: true})
+      }
     }).catch(error => {
       console.error('Error sending friend request:', error);
     });
   }
 
   return(
-    <form className="d-flex position-relative mw-100 pt-3" role="search">
-      <div className="position-relative">
-        <input 
-          onChange={(e) => handleSearch(e)} 
-          type="text" 
-          className="form-control" 
-          placeholder="Search Player" 
-          aria-label="Search"/>
-      </div>
-      {
-        searchQuery.length > 0 && (
-          <div className="pt-5 text-dark">
+    <>
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)} 
+        type="search" 
+        placeholder="Search Player" 
+      />
+        {
+          searchQuery.length > 0 && (
+            <div className="pt-5 text-dark">
+              {
+                searchQuery.map((user, index) => (
+                  <div key={index} className="border border-2 text-dark">
+                    <span>{user.nick_name}</span>
+                    <CustomTooltip text="Send Friend Request" position="bottom">
+                      <button className="btn" onClick={() => handleFriendRequest(session?.user.id, user.id)}>
+                        <PersonAdd color="green" width={15} />
+                      </button>
+                    </CustomTooltip>
+                  </div>
+                ))
+              }
+            </div>)
+        }
+        <ToastContainer
+          className="p-3"
+          position='bottom-center'
+          style={{ zIndex: 1 }}
+        >
+          <Toast 
+            onClose={() => setToastParams((params) => ({...params, show: false}))} 
+            show={toastParams.show} 
+            delay={5000} 
+            autohide
+            bg={toastParams.color}
+            >
+            <Toast.Header className={(toastParams.title === 'Success') ? "text-success" : "text-danger"}>
             {
-              searchQuery.map((user, index) => (
-                <div key={index} className="border border-2 text-dark">
-                  <span>{user.nick_name}</span>
-                  <CustomTooltip text="add friend" position="bottom">
-                    <button className="btn" onClick={() => handleFriendRequest(session?.user.id, user.id)}>
-                      <PersonAdd color="green" width={15} />
-                    </button>
-                  </CustomTooltip>
-                </div>
-              ))
+              (toastParams.title === 'Success') ? (
+                <i className="bi bi-check-circle-fill"></i>
+              ) : (
+                <i className="bi bi-exclamation-triangle-fill"></i>
+              )
             }
-          </div>)
-      }
-  </form>
+            <strong className="ms-1 me-auto">{toastParams.title}</strong>
+            </Toast.Header>
+            <Toast.Body className="text-light">{toastParams.text}</Toast.Body>
+          </Toast>
+        </ToastContainer>
+  </>
   )
 }
