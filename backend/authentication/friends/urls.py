a698476e7184cc6293cc9e71402011c50a88dc3d
Antoineloisea1 <antoineloisea1@gmail.com>
2024-06-12 18:15:56 +0200

    friends are not retrieved by the db

--- backend/authentication/friends/urls.py
+++ backend/authentication/friends/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SendFriendRequestView, ApprouveFriendRequest
+from .views import SendFriendRequestView, ApprouveFriendRequest, GetFriendsView
 
 urlpatterns = [
   path('send-friend-request/', SendFriendRequestView.as_view()),
-  path('approuve-friend-request/', ApprouveFriendRequest.as_view())
+  path('approuve-friend-request/', ApprouveFriendRequest.as_view()),
+  path('get/', GetFriendsView.as_view())
 ]
--- backend/authentication/friends/views.py
+++ backend/authentication/friends/views.py
@@ -3,6 +3,7 @@ from users.models import UserAccount
 from friends.models import Friendship
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Q  
 
 # Create your views here.
 class SendFriendRequestView(APIView):
@@ -13,10 +14,10 @@ class SendFriendRequestView(APIView):
       to_user = UserAccount.objects.get(id=data['to_user'])
     except UserAccount.DoesNotExist:
       return Response(status=status.HTTP_404_NOT_FOUND)
-    requestor = "UID1"
+    requestor = from_user
     if from_user.id > to_user.id:
       from_user, to_user = to_user, from_user
-      requestor = "UID2"
+      requestor = to_user
     friend_request, created = Friendship.objects.get_or_create(user_id1 = from_user, user_id2 = to_user, requestor = requestor, status="REQUEST")
     if created:
       return Response(data={"request_id": friend_request.id}, status=status.HTTP_201_CREATED)
@@ -38,3 +39,29 @@ class ApprouveFriendRequest(APIView):
     friendship.status = "FRIENDS"
     friendship.save()
     return Response(status=status.HTTP_202_ACCEPTED)
+
+class GetFriendsView(APIView):
+  def get(self, request):
+    id = request.query_params.get('id')
+    user = UserAccount.objects.get(id=id)
+    friendships = Friendship.objects.filter(
+        Q(user_id1=user) | Q(user_id2=user)
+      ).exclude(
+        Q(status='REQUEST') & Q(requestor=user)
+      )
+
+    user_ids = list(set([friend.user_id1_id for friend in friendships] + [friend.user_id2_id for friend in friendships]))
+    users = UserAccount.objects.filter(id__in=user_ids)
+
+    # Create a dictionary mapping user IDs to user data
+    user_data = {user.id: {'nick_name': user.nick_name, 'image': user.image.url} for user in users}
+
+    # Serialize friendship data along with user data
+    serialized_data = [
+            {
+                'user': user_data[friend.user_id1_id] if friend.user_id1_id != user.id else user_data[friend.user_id2_id],
+                'status': friend.status,
+            }
+            for friend in friendships
+    ]
+    return Response(serialized_data)
