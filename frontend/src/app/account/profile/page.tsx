d7d49fb5ceee632f19fe6d0b2151216ab38793bb
Antoineloisea1 <antoineloisea1@gmail.com>
2024-06-18 17:10:49 +0200

    starting the profile page

--- frontend/src/app/account/profile/page.tsx
+++ frontend/src/app/account/profile/page.tsx
@@ -9,7 +9,7 @@ import { useRouter } from "next/navigation";
 
 
 
-function ProfilePage(){
+export default function ProfilePage() {
   const { data: session, update} = useSession({required: true});
   const [isEditing, setIsEditing] = useState(false)
   const [data, setData] = useState({
@@ -75,86 +75,118 @@ function ProfilePage(){
       const res = await response.json()
       setData({...data, error: res.message})
     }
-} 
+  }
 
-  if(session) {
-    return (
-      <>
-        <h1>Profile</h1>
-        <h2>{session.user.nick_name}</h2>
-        {isEditing ? (
-          <form onSubmit={updateUsername}>
-            <input type="text" value={data.username} onChange={(e) => setData({...data, username: DOMPurify.sanitize(e.target.value)})} />
-            <button type="submit" className="btn btn-primary">Submit</button>
-            <button type="button" onClick={() => setIsEditing(false)} className="btn btn-primary">Cancel</button>
-          </form>
-        ) : (
-          <button className="btn btn-primary" onClick={() => setIsEditing(true)}>Edit Username</button>
-        )}
-        <p>{session.user.email}</p>
-        <form onSubmit={changePassword}>
-          <input placeholder="Current Password" type="password" value={data.oldPassword} onChange={(e) => setData({...data, oldPassword: DOMPurify.sanitize(e.target.value)})} />
-          <input placeholder="New Password" type="password" value={data.newPassword} onChange={(e) => setData({...data, newPassword: DOMPurify.sanitize(e.target.value)})} />
-          <input placeholder="Confirm Password" type="password" value={data.rePassword} onChange={(e) => setData({...data, rePassword: DOMPurify.sanitize(e.target.value)})} />
-          <button type="submit" className="btn btn-primary">Submit</button>
-          <div className="form-text text-danger">{data.error}</div>
-        </form>
-        {
-              session.user.image ? (
-                <>
-                  <Image className="me-2"
-                    src={`http://backend:8000${session.user.image}`}
-                    width={100}
-                    height={60}
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    alt="42 session picture"
-                  />
-                </>) : (    
-                  <div className="spinner-border spinner-border-sm" role="status">
-                    <span className="sr-only"></span>
-                  </div>
-            )}
-        <ImageUpload />
-          
-
-        <button type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-          Delete Account
-        </button>
-        
-        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h1 className="modal-title fs-5" id="staticBackdropLabel">We are sad to see you leaving 😔</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                <form onSubmit={deleteAccount}>
-                  <label htmlFor="confirm-password-input" className="form-label">Please confirm your password to proceed</label>
-                  <input 
-                    className="form-control" 
-                    id="confirm-password-input" 
-                    placeholder="Password" 
-                    type="password" 
-                    value={data.pwDelete} 
-                    onChange={(e) => setData({...data, pwDelete: DOMPurify.sanitize(e.target.value)})}
-                  />
-                  <div className="form-text text-danger">{data.error}</div>
-                </form>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">I Changed My Mind</button>
-                <button type="submit" onClick={deleteAccount}className="btn btn-danger">See You</button>
-              </div>
+  return(
+    <>
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="card shadow-sm text-center">
+          <div className="card-body">
+            <div className="position-relative">
+              {
+                session.user.image ? (
+                  <>
+                    <Image className="img-fluid rounded-circle"
+                      layout="responsive"
+                      width={768}
+                      height={768}
+                      src={`http://backend:8000${session.user.image}`}
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      alt="42 session picture"
+                      />
+                    <ImageUpload />
+                    </>
+                  ) : (
+                    <div className="spinner-border spinner-border-sm" role="status">
+                      <span className="sr-only"></span>
+                    </div>
+                  )
+}
             </div>
+              <p>{session.user.nick_name}</p>
           </div>
         </div>
-      </>
-    );
-  }
-  else {
-    return(<h1>Error</h1>)
-  }
+      </div>
+    </>
+  )
+
 };
 
-export default ProfilePage;
+// if(session) {
+//   return (
+//     <>
+//       <h1>Profile</h1>
+//       <h2>{session.user.nick_name}</h2>
+//       {isEditing ? (
+//         <form onSubmit={updateUsername}>
+//           <input type="text" value={data.username} onChange={(e) => setData({...data, username: DOMPurify.sanitize(e.target.value)})} />
+//           <button type="submit" className="btn btn-primary">Submit</button>
+//           <button type="button" onClick={() => setIsEditing(false)} className="btn btn-primary">Cancel</button>
+//         </form>
+//       ) : (
+//         <button className="btn btn-primary" onClick={() => setIsEditing(true)}>Edit Username</button>
+//       )}
+//       <p>{session.user.email}</p>
+//       <form onSubmit={changePassword}>
+//         <input placeholder="Current Password" type="password" value={data.oldPassword} onChange={(e) => setData({...data, oldPassword: DOMPurify.sanitize(e.target.value)})} />
+//         <input placeholder="New Password" type="password" value={data.newPassword} onChange={(e) => setData({...data, newPassword: DOMPurify.sanitize(e.target.value)})} />
+//         <input placeholder="Confirm Password" type="password" value={data.rePassword} onChange={(e) => setData({...data, rePassword: DOMPurify.sanitize(e.target.value)})} />
+//         <button type="submit" className="btn btn-primary">Submit</button>
+//         <div className="form-text text-danger">{data.error}</div>
+//       </form>
+//       {
+//             session.user.image ? (
+//               <>
+//                 <Image className="me-2"
+//                   src={`http://backend:8000${session.user.image}`}
+//                   width={100}
+//                   height={60}
+//                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+//                   alt="42 session picture"
+//                 />
+//               </>) : (    
+//                 <div className="spinner-border spinner-border-sm" role="status">
+//                   <span className="sr-only"></span>
+//                 </div>
+//           )}
+//       <ImageUpload />
+        
+
+//       <button type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+//         Delete Account
+//       </button>
+      
+//       <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+//         <div className="modal-dialog modal-dialog-centered">
+//           <div className="modal-content">
+//             <div className="modal-header">
+//               <h1 className="modal-title fs-5" id="staticBackdropLabel">We are sad to see you leaving 😔</h1>
+//               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//             </div>
+//             <div className="modal-body">
+//               <form onSubmit={deleteAccount}>
+//                 <label htmlFor="confirm-password-input" className="form-label">Please confirm your password to proceed</label>
+//                 <input 
+//                   className="form-control" 
+//                   id="confirm-password-input" 
+//                   placeholder="Password" 
+//                   type="password" 
+//                   value={data.pwDelete} 
+//                   onChange={(e) => setData({...data, pwDelete: DOMPurify.sanitize(e.target.value)})}
+//                 />
+//                 <div className="form-text text-danger">{data.error}</div>
+//               </form>
+//             </div>
+//             <div className="modal-footer">
+//               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">I Changed My Mind</button>
+//               <button type="submit" onClick={deleteAccount}className="btn btn-danger">See You</button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+// else {
+//   return(<h1>Error</h1>)
+// }
