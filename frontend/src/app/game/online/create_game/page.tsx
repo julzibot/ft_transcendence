9a9d6c1ad052ec5de2db402e2e352f4c9f3f5cfe
Milan <drownedmilan@gmail.com>
2024-06-23 22:35:00 +0200

    minor create_game UI fixes

--- frontend/src/app/game/online/create_game/page.tsx
+++ frontend/src/app/game/online/create_game/page.tsx
@@ -13,18 +13,29 @@ export default function CreateGame() {
 		<>
 			<div className="d-flex flex-column align-items-center justify-content-center h-100">
 
-				<h2 className="mt-3">Create Game</h2>
+			<div className="container m-3">
+				<div className="row justify-content-center">
 
-				<div className="m-2">
-					<Link href="game/local">
-						<button className="btn btn-danger" type="button">Play With Friends</button>
-					</Link>
+					<div className="col-lg-auto mb-3 d-flex align-items-stretch justify-content-center">
+						<div className="card text-center p-3">
+							<div className="m-3">
+								<h4 className="card-title">Create Game</h4>
+							</div>
+							<div className="m-2">
+							<Link href="game/local">
+								<button className="btn btn-danger" type="button">Play With Friends</button>
+							</Link>
+							</div>
+							<div className="m-2">
+								<Link href="game/online">
+									<button className="btn btn-danger" type="button">Play Online</button>
+								</Link>
+							</div>
+						</div>
+					</div>
+
 				</div>
-				<div className="m-2">
-					<Link href="game/online">
-						<button className="btn btn-danger" type="button">Play Online</button>
-					</Link>
-				</div>
+			</div>
 
 			</div>
 		</>
