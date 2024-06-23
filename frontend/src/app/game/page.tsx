561db488c5940e99e15275e8d315cdeb9209ba0f
Milan <drownedmilan@gmail.com>
2024-06-23 22:10:06 +0200

    minor UI fixes

--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -13,17 +13,47 @@ export default function GameMain() {
 		<>
 			<div className="d-flex flex-column align-items-center justify-content-center h-100">
 
-				<h2 className="mt-3">Game Mainpage</h2>
+				<h2 className="m-3 text-decoration-underline">Game Mainpage</h2>
+				<div className="container">
 
-				<div className="m-2">
-					<Link href="game/local">
-						<button className="btn btn-primary">Local Game</button>
-					</Link>
-				</div>
-				<div className="m-2">
-					<Link href="game/online">
-						<button className="btn btn-primary">Online Game</button>
-					</Link>
+					<div className="row justify-content-center">
+						<div className="col-lg-auto mb-3 d-flex align-items-stretch justify-content-center">
+							<div className="card text-center">
+								<div className="m-3">
+									<h5 className="card-title">Local Game</h5>
+								</div>	
+								<div className="card-body d-flex flex-column">
+									<div className="m-3 d-flex align-items-center justify-content-center">
+										<Link href="game/local">
+											<button className="btn btn-primary">Play</button>
+										</Link>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-lg-auto mb-3 d-flex align-items-stretch justify-content-center">
+						
+							<div className="card text-center">
+								<div className="card-body d-flex flex-column">
+									<h5 className="card-title">Online Game</h5>
+									<div className="input-group mt-3">
+										<input type="text" className="form-control" placeholder="Enter Game ID" aria-label="Join Online Game" aria-describedby="button-addon2" />
+										<Link href="game/local">
+											<button className="btn btn-outline-secondary" type="button" id="button-addon2">Join Game</button>
+										</Link>
+									</div>
+									<h5 className="m-2">or</h5>
+									<div className="m-2">
+										<Link href="game/online/create_game">
+											<button className="btn btn-warning">Create Game</button>
+										</Link>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
 				</div>
 
 			</div>
