60b5a70966ff3542aca88fef0dfbc7056ed98e3c
Milan <drownedmilan@gmail.com>
2024-06-24 18:47:13 +0200

    local working smoothly now

--- frontend/src/app/game/local/play/page.tsx
+++ frontend/src/app/game/local/play/page.tsx
@@ -2,16 +2,14 @@
 
 import Join from "../../../../components/game/Join"
 import { useEffect, useState, useContext } from "react"
-import io from "socket.io-client"
 import { SocketContext, socket } from "../../../../../context/socket"
-import '../custom.scss'
 
 export default function Play() {
 
 	return (
 	<>
 		<SocketContext.Provider value={socket}>
-			<Join />
+			<Join remoteGame={false} />
 		</SocketContext.Provider>
 	</>
 	)
