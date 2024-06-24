ceb6bf107c1852fb9cd88ee610daead70aca211c
Milan <drownedmilan@gmail.com>
2024-06-24 17:55:58 +0200

    UI yassification + gameSettings properly passed

--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -6,6 +6,7 @@ import { useSession } from "next-auth/react"
 import { useEffect, useState, useContext } from "react"
 import io from "socket.io-client"
 import { SocketContext, socket } from "../../../../context/socket"
+import "./styles.css"
 
 export default function GameMain() {
 
@@ -18,18 +19,36 @@ export default function GameMain() {
 
 					<div className="row justify-content-center">
 						<div className="col-lg-auto mb-3 d-flex align-items-stretch justify-content-center">
-							<div className="card text-center">
+							{/* <div className="card text-center">
 								<div className="m-3">
 									<h5 className="card-title">Local Game</h5>
 								</div>	
 								<div className="card-body d-flex flex-column">
 									<div className="m-3 d-flex align-items-center justify-content-center">
 										<Link href="game/local">
-											<button className="btn btn-primary">Play</button>
+											<button className="btn btn-lg btn-primary">Play</button>
 										</Link>
 									</div>
 								</div>
-							</div>
+							</div> */}
+							<div className="card l-bg-cherry">
+							<Link href="game/local" style={{color: 'white', textDecoration: 'none'}}>
+                <div className="card-statistic-3 p-4">
+                    <div className="card-icon card-icon-large"><i className="fas fa-shopping-cart"></i></div>
+                    <div className="mb-4 card-text">
+                        <h5 className="card-title mb-0">Offline Mode</h5>
+                    </div>
+                    <div className="row align-items-center mb-2 d-flex">
+                        <div className="col-8 card-text">
+                            <h2 className="d-flex align-items-center mb-0">
+                                Play Locally
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+						</Link>
+            </div>
+
 						</div>
 
 						<div className="col-lg-auto mb-3 d-flex align-items-stretch justify-content-center">
@@ -37,12 +56,16 @@ export default function GameMain() {
 							<div className="card text-center">
 								<div className="card-body d-flex flex-column">
 									<h5 className="card-title">Online Game</h5>
+
 									<div className="input-group mt-3">
-										<input type="text" className="form-control" placeholder="Enter Game ID" aria-label="Join Online Game" aria-describedby="button-addon2" />
+										<input type="text" className="form-control shadow-none rounded" placeholder="Enter Game ID" aria-label="Join Online Game" aria-describedby="button-addon2" />
 										<Link href="game/local">
-											<button className="btn btn-outline-secondary" type="button" id="button-addon2">Join Game</button>
+											<button className="btn btn-outline-secondary ms-1" type="button" id="button-addon2">Join Game</button>
 										</Link>
 									</div>
+
+									
+
 									<h5 className="m-2">or</h5>
 									<div className="m-2">
 										<Link href="game/online/create_game">
