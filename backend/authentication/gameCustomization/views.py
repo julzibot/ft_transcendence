27a2ef1f847dd03470b26bf27038469fcd78fba8
Milan <drownedmilan@gmail.com>
2024-07-03 18:00:34 +0200

    Game Customization front + back is working!

--- backend/authentication/gameCustomization/views.py
+++ backend/authentication/gameCustomization/views.py
@@ -10,26 +10,24 @@ from .serializers import GameCustomizationSerializer
 # Create your views here.
 class GameCustomizationView(APIView):
 	def get(self, request, id):
-		settings = GameCustomizationData.objects.filter(user_id=id)
-		if settings.exists():
-			serializer = GameCustomizationSerializer(settings, many=True)
+		try:
+			settings = GameCustomizationData.objects.get(user_id=id)
+			serializer = GameCustomizationSerializer(settings)
 			return Response({'data': serializer.data}, status=status.HTTP_200_OK)
-		else:
+		except GameCustomizationData.DoesNotExist:
 			return Response({'data': '404 Not Found'}, status=status.HTTP_404_NOT_FOUND)
 
 class UpdateGameCustomizationView(APIView):
 	def post(self, request):
 		data = request.data
-		user = get_object_or_404(UserAccount, id=data['user_id'])
 
-		objs = GameCustomizationData.objects.filter(user_id=data['user_id'])
+		user = get_object_or_404(UserAccount, id=data['userId'])
 
-		if (objs.exists()):
-			for obj in objs:
-				obj.background = data['background']
-				obj.palette = data['palette']
-				obj.bgColor = data['bgColor']
-				obj.opacity = data['opacity']
-				obj.sparks = data['sparks']
-				obj.save()
-			return Response({'message': 'Game customization settings updated successfully'}, status=status.HTTP_202_ACCEPTED)
+		obj = GameCustomizationData.objects.get(user_id=data['userId'])
+		obj.background = data['background']
+		obj.palette = data['palette']
+		obj.bgColor = data['bgColor']
+		obj.opacity = data['opacity']
+		obj.sparks = data['sparks']
+		obj.save()
+		return Response({'message': 'Game customization settings updated successfully'}, status=status.HTTP_202_ACCEPTED)
--- frontend/src/app/account/profile/[id]/page.tsx
+++ frontend/src/app/account/profile/[id]/page.tsx
@@ -124,7 +124,7 @@ function ProfilePage(props: Props){
           Delete Account
         </button>
         
-        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
           <div className="modal-dialog modal-dialog-centered">
             <div className="modal-content">
               <div className="modal-header">
