91deeca0f45842bf7f0a3696842884de54302fd4
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-07-12 17:22:57 +0200

    preparing to work on friend list

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,43 +1,16 @@
 'use client';
 
-import { useState } from 'react';
-import { Button, Offcanvas } from 'react-bootstrap';
-import FriendList from "@/components/ui/friend_list/FriendList";
 import GameCard from '@/components/cards/GameCard';
 
 
 export default function Home() {
-  const [show, setShow] = useState(false);
 
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <>
       <div className="d-flex flex-row justify-content-center mt-5 pt-5">
         <GameCard src="/pong.mov" />
       </div>
-      {/* <Offcanvas
-        show={show}
-        onHide={handleClose}
-        placement="end"
-        scroll={true}
-      > */}
-        {/* <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Friends List</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <FriendList />
-        </Offcanvas.Body>
-      {/* </Offcanvas>
-      <Button
-        variant="primary"
-        className="position-fixed bottom-0 end-0 m-4"
-        onClick={handleShow}
-        >
-        Friends List
-      </Button> */} */}
     </>
   )
 }
--- frontend/src/components/ui/navbar/Navbar.tsx
+++ frontend/src/components/ui/navbar/Navbar.tsx
@@ -2,52 +2,54 @@
 import Link from "next/link"
 import AuthButton from "./AuthButton"
 import Image from "next/image"
-import { useGameContext } from "@/app/context/GameContext"
+import { useState } from "react";
 import "./styles.css"
+import FriendList from "../friend_list/FriendList";
+import { Offcanvas } from 'react-bootstrap'
 
 export default function Navbar() {
+  const [show, setShow] = useState(false);
 
-	const { game } = useGameContext();
 
   return (
-	<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div className="container-fluid">
-			<Link href="/" className="ms-2 text-decoration-none navbar-brand">
-			<Image
-				src="/static/images/42.png"
-				style={{filter: "invert(100%)"}}
-				width={35}
-				height={35}
-				alt="42 Logo"
-			/>
-			<span className="ms-2 text-light fw-bold">Transcendence</span>
-			</Link>
-			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="collapse navbar-collapse custom-collapse" id="navbarSupportedContent">
-				<ul className="navbar-nav me-auto mb-lg-0">
-					{
-						game && (
-						<>
-						<li className="nav-item custom-item d-flex align-items-center text-light px-2">
-								{game}
-						</li>
-						<li className="nav-item custom-item d-flex align-items-center px-2">
-							<Link href="/game">
-								<button className="btn btn-info">Play</button>
-							</Link>
-						</li>
-						<li className="nav-item custom-item d-flex align-items-center px-2">
-							<Link href="/chat/test">
-								<button className="btn btn-info">Chat</button>
-							</Link>
-						</li>
-						</>)
-					}
-				</ul>
+		<nav className="navbar navbar-expand-lg bg-dark">
+			<div className="container-fluid justify-content-between">
+
+				<div className="navbar-brand">
+					<Link href="/" className="ms-2 text-decoration-none">
+						<Image
+							src="/static/images/42.png"
+							style={{filter: "invert(100%)"}}
+							width={35}
+							height={35}
+							alt="42 Logo"
+							/>
+						<span className="ms-1 text-light fw-semibold">Transcendence</span>
+					</Link>
+				</div>
+
+				<button
+					className="btn btn-primary btn-lg"
+					onClick={() => setShow(true)}
+					>
+					Friends
+				</button>
+				<Offcanvas
+					show={show}
+					onHide={() => setShow(false)}
+					placement="end"
+					scroll={false}
+					> 
+					<Offcanvas.Header closeButton>
+						<Offcanvas.Title>Friend List</Offcanvas.Title>
+					</Offcanvas.Header>
+					<Offcanvas.Body>
+						<FriendList />
+					</Offcanvas.Body>
+				</Offcanvas>
+
 				<AuthButton/>
 			</div>
-		</div>
-	</nav>);
+		</nav>
+	);
 }
