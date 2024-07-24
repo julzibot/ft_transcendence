d4d7730aa9654f7fa42387d9342aa7672abb14ca
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-07-24 16:56:25 +0200

    signout modal now set is_active to False

--- backend/core/users/models.py
+++ backend/core/users/models.py
@@ -55,7 +55,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
 
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
--- frontend/src/lib/auth/options.ts
+++ frontend/src/lib/auth/options.ts
@@ -55,7 +55,8 @@ export const authOptions: NextAuthOptions = {
   ],
 
   pages: {
-    signIn: "/auth/signin"
+    signIn: "/auth/signin",
+    signOut: "/auth/signout"
   },
 
   callbacks: {
