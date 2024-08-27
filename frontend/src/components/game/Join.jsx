5a8a4467dcc146b768b70f1532f0c6013408e83f
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-08-27 15:03:14 +0200

    Lobby can now lauch games

--- frontend/src/components/game/Join.jsx
+++ frontend/src/components/game/Join.jsx
@@ -3,20 +3,21 @@
 import { useEffect, useState, useContext } from "react";
 import { useSocketContext } from "../../context/socket";
 import ThreeScene from './Game';
+import { Spinner } from 'react-bootstrap';
 
 // Join is a component for Online Game
-export default function Join({ gameMode, gameSettings, userId }) {
+export default function Join({ userId, room, gameSettings, gameMode}) {
 
 	const socket = useSocketContext();
 
 		const [gameJoined, setGameJoined] = useState(false);
 		const [isHost, setIsHost] = useState(false);
 		const [player2_id, setPlayer2_id] = useState(null);
-		const room_id = 5;
+
 
 		useEffect(() => {
-			socket.emit('join_room', { room_id: room_id, user_id: userId });
-			console.log(`[${userId}] Joining room: ${room_id}`)
+			socket.emit('join_room', { room_id: room, user_id: userId });
+			console.log(`[${userId}] Joining room: ${room}`)
 
 			socket.on('isHost', () => {
 				setIsHost(true);
@@ -48,9 +49,16 @@ export default function Join({ gameMode, gameSettings, userId }) {
 			<>
 				{userId && gameJoined ? (
 						// gamemode hardcoded to 2 for now - speak to Jules about handling online/tournament modes
-						<ThreeScene gameSettings={gameSettings} room_id={room_id} user_id={userId} player2_id={player2_id} isHost={isHost} gamemode={2} />
+						<ThreeScene gameSettings={gameSettings} room_id={room} user_id={userId} player2_id={player2_id} isHost={isHost} gamemode={gameMode} />
 					) : (
-						<div>Loading game...</div>
+						<div className="d-flex justify-content-center align-items-center vh-100 text-light">
+							<div className="flex-row align-items-center">
+								<h1>Waiting for an opponent...</h1>
+								<div className="p-5 text-primary" style={{marginLeft: "41px", marginBottom: "19px"}}>
+								<Spinner animation="border" style={{ width: '15rem', height: '15rem', borderWidth: "45px", borderRightColor: "#ff0000", borderTopRightRadius:"75px", borderTopColor: '#26cc00', borderBottomColor: '#ffd700', animationDuration: "3s"}} />
+								</div>
+							</div>
+						</div>
 					)
 				}
 			</>
