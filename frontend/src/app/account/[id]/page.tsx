d54aac79562aa4fdf328700ce68431aa4e01028d
Antoineloiseau1 <antoineloiseau1@gmail.com>
2024-08-29 14:30:38 +0200

    profile pages are accessible from friend list

--- frontend/src/app/account/[id]/page.tsx
+++ frontend/src/app/account/[id]/page.tsx
@@ -7,11 +7,24 @@ import ImageUpload from "@/components/Utils/ImageUpload";
 import DOMPurify from 'dompurify'
 import UserDashboardCard from "@/components/ui/dashboard/UserDashboardCard"
 import Customization from "@/components/game/Customization";
+import { useParams, useRouter } from "next/navigation";
+
+interface User {
+	id: number;
+	username: string;
+	image: string;
+}
 
 export default function ProfilePage() {
 	const [isEditing, setIsEditing] = useState(false)
-	const { data: session, status, update } = useSession();
-
+	const { data: session, update } = useSession();
+	const { id } = useParams()
+	const router = useRouter()
+	const [user, setUser] = useState<any>({
+		id: 0,
+		username: '',
+		image: '',
+	})
 	const [showModal, setShowModal] = useState<boolean>(false)
 	const [isEditPw, setIsEditPw] = useState(false)
 	const initialState = {
@@ -22,8 +35,14 @@ export default function ProfilePage() {
 		usernameError: '',
 		error: '',
 	}
-	const [data, setData] = useState({ initialState })
-
+	const [data, setData] = useState({
+		username: '',
+		oldPassword: '',
+		newPassword: '',
+		rePassword: '',
+		usernameError: '',
+		error: '',
+	})
 
 	const [gameSettings, setGameSettings] = useState({
 		background: 0, // 0 - 3 animated, 4 - 5 static
@@ -44,6 +63,23 @@ export default function ProfilePage() {
 			setShowModal(false);
 		}, 2000);
 	};
+
+	async function getUserInfo() {
+		const response = await fetch(`http://localhost:8000/api/user/get-user-info/?id=${id}`, {
+			method: 'GET',
+			headers: { 'Content-Type': 'application/json' }
+		})
+		if(!response.ok) {
+			router.push(`/${id}`)
+			return;
+		}
+		const data = await response.json()
+		setUser({
+			id: data.user.id,
+			username: data.user.username,
+			image: data.user.image
+		})
+	}
 
 	async function changePassword(event: FormEvent<HTMLFormElement>) {
 		event.preventDefault()
@@ -90,6 +126,7 @@ export default function ProfilePage() {
 		setIsEditing(false);
 	}
 
+
 	async function deleteAccount() {
 		const response = await fetch('http://localhost:8000/api/auth/user/delete/', {
 			method: 'DELETE',
@@ -107,6 +144,10 @@ export default function ProfilePage() {
 		}
 	}
 
+	useEffect(() => {
+		getUserInfo()
+	}, [])
+
 	return (
 		<>
 			<div className="d-flex flex-row align-items-center justify-content-evenly">
@@ -115,11 +156,11 @@ export default function ProfilePage() {
 						<div className="card-body">
 							<div className="position-relative border border-4 border-dark-subtle rounded-circle" style={{ width: '280px', height: '280px', overflow: 'hidden' }}>
 								{
-									session?.user.image ? (
+									user.image ? (
 										<>
 											<Image style={{ objectFit: 'cover' }}
 												fill
-												src={`http://backend:8000${session.user.image}`}
+												src={`http://backend:8000${user.image}`}
 												alt="Profile Picture"
 												priority={true}
 												sizes="25vw"
@@ -132,111 +173,129 @@ export default function ProfilePage() {
 									)
 								}
 							</div>
-							<ImageUpload />
-							<br />
-							<div className="d-flex flex-row justify-content-center align-items-center">
-								{
-									isEditing ? (
-										<>
-											<form onSubmit={updateUsername}>
-												<input type="text" className="form-control form-control-sm" value={data.username} onChange={(e) => setData({ ...data, username: DOMPurify.sanitize(e.target.value) })} />
-												<button type="submit" className="btn btn-primary btn-sm rounded-pill">Submit</button>
-												<button type="button" onClick={() => setIsEditing(false)} className="btn btn-secondary rounded-pill btn-sm">Cancel</button>
-											</form>
-										</>
-									) : (
-										<>
-											<div className="container align-items-center">
-												<div className="row">
-													{
-														session.user.username ? (
-															<>
-																<span className="col-9 card-text fw-semibold fs-4">{session?.user.username}</span>
-															</>
-														) : (
-															<>
-																<div className="col-9">
-																	<p className="card-text fs-4 placeholder-glow">
-																		<span className="placeholder col-7"></span>
-																	</p>
-																</div>
-															</>
-														)
-													}
-													{
-														session.user.id ? (
-															<button className="col-2 btn btn-sm btn-primary rounded-pill" onClick={() => setIsEditing(true)}>Edit</button>
-
-														) : (
-															<a className="col-2 btn btn-sm btn-primary rounded-pill disabled placeholder" aria-disabled="true">Edit</a>
-														)
-													}
-												</div>
-											</div>
-										</>
-									)
-								}
-							</div>
-							<div className="text-danger">{data.usernameError}</div>
-							<hr />
+								<br />
 							{
-								session.user.email ? (
-									<span className="card-subtitle text-body-secondary fw-semibold">{session?.user.email}</span>
-								) : (
-									<p className="card-subtitle placeholder-glow">
-										<span className="placeholder col-7"></span>
-									</p>
-								)
-							}
-							<hr />
-							{
-								session.user.id ? <Customization updateSettings={setGameSettings} gameSettings={gameSettings} userId={session.user.id} /> : (
-									<a className="col btn btn-primary rounded-pill disabled placeholder" aria-disabled="true">Game Customization</a>
-								)
-							}
-							{
-								session?.provider !== '42-school' && (
+								session?.user.id === Number(id) ? (
 									<>
-										<hr />
+										<ImageUpload />
 										<div className="d-flex flex-row justify-content-center align-items-center">
 											{
-												isEditPw ? (
+												isEditing ? (
 													<>
-														<form onSubmit={changePassword}>
-															<input placeholder="Current Password" className="form-control form-control-sm" type="password" value={data.oldPassword} onChange={(e) => setData({ ...data, oldPassword: DOMPurify.sanitize(e.target.value) })} />
-															<input placeholder="New Password" className="form-control form-control-sm" type="password" value={data.newPassword} onChange={(e) => setData({ ...data, newPassword: DOMPurify.sanitize(e.target.value) })} />
-															<input placeholder="Confirm Password" className="form-control form-control-sm" type="password" value={data.rePassword} onChange={(e) => setData({ ...data, rePassword: DOMPurify.sanitize(e.target.value) })} />
+														<form onSubmit={updateUsername}>
+															<input type="text" className="form-control form-control-sm" value={data.username} onChange={(e) => setData({ ...data, username: DOMPurify.sanitize(e.target.value) })} />
 															<button type="submit" className="btn btn-primary btn-sm rounded-pill">Submit</button>
-															<button type="button" onClick={() => setIsEditPw(false)} className="btn btn-secondary rounded-pill btn-sm">Cancel</button>
-															<div className="form-text text-danger">{data.error}</div>
+															<button type="button" onClick={() => setIsEditing(false)} className="btn btn-secondary rounded-pill btn-sm">Cancel</button>
 														</form>
 													</>
 												) : (
 													<>
-														{
-															session.user.id ? (
-																<button className='btn btn-primary btn-sm rounded-pill' onClick={() => setIsEditPw(true)}>Change Password</button>
-															) : (
-																<a className='btn btn-primary btn-sm rounded-pill disabled placeholder' aria-disabled="true">Change Password</a>
-															)
-														}
+														<div className="container align-items-center">
+															<div className="row">
+																{
+																	session.user.username ? (<span className="col-9 card-text fw-semibold fs-4">{session.user.username}</span>) : (
+																		// Display placeholder if username is not set
+																		<>
+																			<div className="col-9">
+																				<p className="card-text fs-4 placeholder-glow">
+																					<span className="placeholder col-7"></span>
+																				</p>
+																			</div>
+																		</>
+																	)
+																}
+																{
+																	session.user.id ? ( <button className="col-2 btn btn-sm btn-primary rounded-pill" onClick={() => setIsEditing(true)}>Edit</button>) : (
+																		<a className="col-2 btn btn-sm btn-primary rounded-pill disabled placeholder" aria-disabled="true">Edit</a>
+																	)
+																}
+															</div>
+														</div>
 													</>
 												)
+											}
+										</div>
+										<div className="text-danger">{data.usernameError}</div>
+									</>
+									) : (
+										<>
+											{
+												user.username ? ( <span className="col card-text fw-semibold fs-4">{user.username}</span> ) : (
+													<>
+														<div className="col">
+															<p className="card-text fs-4 placeholder-glow">
+																<span className="placeholder col-7"></span>
+															</p>
+														</div>
+													</>
+												)
+											}
+										</>
+								)
+							}
+							<hr />
+							{
+								session?.user.id == Number(id) ? (
+									<>
+										{
+											session?.user.email ? ( <span className="card-subtitle text-body-secondary fw-semibold">{session?.user.email}</span>) : (
+												<p className="card-subtitle placeholder-glow">
+													<span className="placeholder col-7"></span>
+												</p>
+											)
+										}
+										<hr />
+										{
+											session?.user.id ? <Customization updateSettings={setGameSettings} gameSettings={gameSettings} userId={session.user.id} /> : (
+												<a className="col btn btn-primary rounded-pill disabled placeholder" aria-disabled="true">Game Customization</a>
+											)
+										}
+										{
+											session?.provider !== '42-school' && (
+												<>
+													<hr />
+													<div className="d-flex flex-row justify-content-center align-items-center">
+														{
+															isEditPw ? (
+																<>
+																	<form onSubmit={changePassword}>
+																		<input placeholder="Current Password" className="form-control form-control-sm" type="password" value={data.oldPassword} onChange={(e) => setData({ ...data, oldPassword: DOMPurify.sanitize(e.target.value) })} />
+																		<input placeholder="New Password" className="form-control form-control-sm" type="password" value={data.newPassword} onChange={(e) => setData({ ...data, newPassword: DOMPurify.sanitize(e.target.value) })} />
+																		<input placeholder="Confirm Password" className="form-control form-control-sm" type="password" value={data.rePassword} onChange={(e) => setData({ ...data, rePassword: DOMPurify.sanitize(e.target.value) })} />
+																		<button type="submit" className="btn btn-primary btn-sm rounded-pill">Submit</button>
+																		<button type="button" onClick={() => setIsEditPw(false)} className="btn btn-secondary rounded-pill btn-sm">Cancel</button>
+																		<div className="form-text text-danger">{data.error}</div>
+																	</form>
+																</>
+															) : (
+																<>
+																	{
+																			session?.user.id ? ( <button className='btn btn-primary btn-sm rounded-pill' onClick={() => setIsEditPw(true)}>Change Password</button>) : (
+																				<a className='btn btn-primary btn-sm rounded-pill disabled placeholder' aria-disabled="true">Change Password</a>
+																		)
+																	}
+																</>
+															)
 											}
 										</div>
 									</>)
 							}
 							<hr />
 							{
-								session.user.id ? (
+								session?.user.id ? (
 									<button type="button" className="btn btn-danger rounded-pill" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Delete Account</button>
 								) : (
 									<a className='btn btn-danger rounded-pill disabled placeholder' aria-disabled="true">Delete Account</a>
 								)
 							}
+									</>
+								) : (<></>) 
+							}
 						</div>
 					</div>
-				</div>
+					</div>
+
+
 
 				<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
 					<div className="modal-dialog modal-dialog-centered">
@@ -270,7 +329,9 @@ export default function ProfilePage() {
 						</div>
 					</div>
 				</div>
-				<UserDashboardCard user_id={session.user.id} />
+				{
+					user && <UserDashboardCard user={user}/>
+				}
 			</div >
 		</>
 	)
