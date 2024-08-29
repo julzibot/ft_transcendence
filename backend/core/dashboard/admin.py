b5fcc1286e2444a009a42f427108aecac8fcebc2
Milan <drownedmilan@gmail.com>
2024-08-29 18:03:34 +0200

    game match display users

--- backend/core/dashboard/admin.py
+++ backend/core/dashboard/admin.py
@@ -4,6 +4,17 @@ from django.contrib import admin
 from .models import DashboardData
 
 class DashboardAdmin(admin.ModelAdmin):
+	fields = ['user', 'record_streak', 'tournaments_won', 'tournaments_played', 'games_played', 'wins']
+	list_display = ['user', 'record_streak', 'tournaments_won', 'tournaments_played', 'games_played']
 	readonly_fields = ['games_played', 'wins']
-	fields = ['user', 'record_streak', 'tournaments_won', 'tournaments_played']
+
+	def wins(self, obj):
+		return obj.wins
+	
+	def games_played(self, obj):
+		return obj.games_played
+	
+	wins.short_description = 'Wins'
+	games_played.short_description = 'Games Played'
+
 admin.site.register(DashboardData, DashboardAdmin)
