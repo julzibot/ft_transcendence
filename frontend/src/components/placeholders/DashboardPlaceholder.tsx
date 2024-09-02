ffc78b94fa2ea065d751d44dd3f0d11bdfa980c0
Antoineloisea1 <antoineloisea1@gmail.com>
2024-09-02 15:45:38 +0200

    fixed account divs

--- frontend/src/components/placeholders/DashboardPlaceholder.tsx
+++ frontend/src/components/placeholders/DashboardPlaceholder.tsx
@@ -1,24 +1,18 @@
 export function DashboardPlaceholder() {
   return (
     <>
-      <ul className="list-unstyled">
-        <li>
-          <p className="card-text placeholder-glow">
-            <span>Wins: </span>
-            <span className="placeholder col-1"></span>
-          </p>
-        </li>
-        <li>
-          <p className="card-text placeholder-glow">
-            <span>Loses: </span>
-            <span className="placeholder col-1"></span>
-          </p>
-        </li>
-      </ul >
-      <p className="card-text placeholder-glow">
-        <span>Current Streak Record: </span>
+      <span className="placeholder-glow">
+        <span>Wins: </span>
+        <span className="placeholder col-1"></span>
+      </span>
+      <p className="placeholder-glow">
+        <span>Loses: </span>
         <span className="placeholder col-1"></span>
       </p>
+      <span className="card-text placeholder-glow">
+        <span>Current Streak Record: </span>
+        <span className="placeholder col-1"></span>
+      </span>
       <p className="card-text placeholder-glow">
         <span>Number Of Matches Played: </span>
         <span className="placeholder col-1"></span>
@@ -33,20 +27,21 @@ export function DashboardPlaceholder() {
       <a className="btn btn-primary disabled placeholder col-1 m-2" aria-disabled="true"></a>
       <a className="btn btn-primary disabled placeholder col-1 m-2" aria-disabled="true"></a>
       <a className="btn btn-secondary disabled placeholder col-1 m-2" aria-disabled="true"></a>
-      <div className='container m-3'>
-        <div className="row align-items-center justify-content-between">
-          <div className="col-6">
-            <div className="position-relative" style={{ width: '500px', height: '500px' }}>
-              <div className="placeholder-glow w-100 h-100">
-                <div className="placeholder bg-secondary w-100 h-100"></div>
-              </div>
+
+      <div className='d-flex flex-row m-3 align-items-center justify-content-evenly' >
+        <div className="flex-column text-center">
+          <label className="text-secondary fw-bold fs-5">Activity Chart</label>
+          <div className="position-relative" style={{ width: '500px', height: '300px' }}>
+            <div className="placeholder-glow w-100 h-100">
+              <div className="placeholder bg-secondary w-100 h-100"></div>
             </div>
           </div>
-          <div className="col-6">
-            <div className="position-relative rounded-circle" style={{ width: '500px', height: '500px', overflow: 'hidden' }}>
-              <div className="placeholder-glow w-100 h-100">
-                <div className="placeholder bg-secondary w-100 h-100"></div>
-              </div>
+        </div>
+        <div className=" ms-5 flex-column text-center">
+          <label htmlFor="" className="text-secondary fw-bold fs-5">Game Mode Chart</label>
+          <div className="position-relative rounded-circle" style={{ width: '300px', height: '300px', overflow: 'hidden' }}>
+            <div className="placeholder-glow w-100 h-100">
+              <div className="placeholder bg-secondary w-100 h-100"></div>
             </div>
           </div>
         </div>
