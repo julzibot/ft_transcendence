135a95f5b66cf39e8b83724b9abc5fc339c86428
Milan <drownedmilan@gmail.com>
2024-09-06 00:06:04 +0200

    FUCKING WORKING

--- frontend/src/components/game/Join.tsx
+++ frontend/src/components/game/Join.tsx
@@ -22,8 +22,10 @@ export default function Join({ userId, room, gameSettings, gameMode }: JoinProps
 
 	const [gameJoined, setGameJoined] = useState(false);
 	const [isHost, setIsHost] = useState(false);
+	const [isNotHost, setIsNotHost] = useState(false);
 	const [player2_id, setPlayer2_id] = useState(null);
-	const [gameCreated, setGameCreated] = useState(false);
+	const [gameCreated, setGameCreated] = useState(false); // POST game to backend
+	const [matchFetched, setMatchFetched] = useState(false); // If gameCreated, GET match infos
 
 	const [gameInfos, setGameInfos] = useState({
 		game_id: -1,
@@ -33,23 +35,31 @@ export default function Join({ userId, room, gameSettings, gameMode }: JoinProps
 		p2p: ''
 	})
 
-
 	useEffect(() => {
 		socket.emit('join_room', { room_id: room, user_id: userId });
-		console.log(`[${userId}] Joining room: ${room}`)
+		console.log(`[${room}] ${userId} has joined`)
 
 		socket.on('isHost', () => {
 			setIsHost(true);
-			console.log("You are player 1");
+			console.log(`[${room}] You are player 1 [HOST]`);
 		});
-		
-		if (isHost) {
-			socket.on('player2_id', (data) => {
-				setPlayer2_id(data.player2_id);
-			});
-		}
+
+		socket.on('isNotHost', () => {
+			setIsNotHost(true);
+			console.log(`[${room}] You are player 2 [NOT HOST]`);
+		})
+
+		socket.on('player2_id', (data) => {
+			setPlayer2_id(data.player2_id);
+			console.log(`[socket] data.player2_id ${data.player2_id}`);
+		});
+
+		socket.on('receiveGameId', (data) => {
+			setGameInfos({ ...gameInfos, game_id: data.game_id });
+		})
 
 		socket.on('startGame', () => {
+			console.log('Start the game!');
 			setGameJoined(true);
 		});
 
@@ -60,68 +70,74 @@ export default function Join({ userId, room, gameSettings, gameMode }: JoinProps
 		};
 	}, [socket]);
 
-	const fetchGameInfos = async (game_id : number) => {
-		const response = await fetch(BASE_URL + `game/history/${game_id}`, {
-			method: 'GET',
-			headers: { 'Content-Type': 'application/json' }
-		})
-		if (response.ok) {
-			const res = await response.json();
-			const data = res.data;
-			setGameInfos({...gameInfos,
-				p1Name: data.player1.username,
-				p2Name: data.player2.username,
-				p1p: data.player1.image,
-				p2p: data.player2.image
-			})
-		}
-		else
-			console.log('[Join] [fetchGameInfos] Error fetching: ' + response.status);
-	}
-
 	useEffect(() => {
-		if (isHost && !gameCreated) {
+		console.log('[useEffect] before if - isHost [' + isHost + '] gameCreated [' + gameCreated + '] player2ID [' + player2_id + ']');
+		if (isHost && player2_id && !gameCreated) {
 			const createGame = async () => {
 				console.log('[Join] CreateGame called');
-					const response = await fetch(BASE_URL + 'game/create', {
+				const response = await fetch(BASE_URL + 'game/create', {
 					method: 'POST',
 					headers: { 'Content-Type': 'application/json' },
 					body: JSON.stringify({
-					'player1': userId,
-					'player2': player2_id,
-					'game_mode': 2
+						'player1': userId,
+						'player2': player2_id,
+						'game_mode': 2
 					})
 				});
 				if (response.status === 201) {
 					const res = await response.json();
-					setGameInfos({...gameInfos, game_id: parseInt(res.id)});
-					fetchGameInfos(gameInfos.game_id);
+					console.log(`Game ID: ${res.id}`);
+					setGameInfos({ ...gameInfos, game_id: parseInt(res.id) });
+					socket.emit('sendGameId', { room_id: room, game_id: res.id });
+					setGameCreated(true); // now GET match info
 				}
 				else
 					console.log('[Join] Error: ' + response.status);
 			}
 			createGame();
-			setGameCreated(true);
 		}
-	}, [player2_id]);
+	}, [isHost, player2_id]);
+
+	useEffect(() => {
+		console.log('[useEffect] before if - gameCreated [' + gameCreated + '] matchFetched [' + matchFetched + ']');
+		if (isNotHost || (gameCreated && !matchFetched)) {
+			console.log('[useEffect] after if');
+			const fetchGameInfos = async (game_id: number) => {
+				const response = await fetch(BASE_URL + `game/history/${game_id}`, {
+					method: 'GET',
+					headers: { 'Content-Type': 'application/json' }
+				})
+				if (response.ok) {
+					const res = await response.json();
+					const data = res.data;
+					setGameInfos({
+						...gameInfos,
+						p1Name: data.player1.username,
+						p2Name: data.player2.username,
+						p1p: data.player1.image,
+						p2p: data.player2.image
+					});
+					socket.emit('fetchFinished', { room_id: room });
+					setMatchFetched(true);
+				}
+				else
+					console.log('[Join] [fetchGameInfos] Error fetching: ' + response.status);
+			}
+			fetchGameInfos(gameInfos.game_id);
+		}
+		// send to socket.io server to start the game
+	}, [gameCreated, gameInfos.game_id]);
 
 	return (
 		<>
 			{userId && gameJoined ? (
-				// gamemode hardcoded to 2 for now - speak to Jules about handling online/tournament modes
-
-				// game_id
-				// player1 & player2 = username + image
-				<ThreeScene gameSettings={gameSettings} room_id={room} user_id={userId} player2_id={player2_id} isHost={isHost} gamemode={gameMode} />
+				<p>{JSON.stringify(gameInfos)}</p>
+				// <ThreeScene gameInfos={gameInfos} gameSettings={gameSettings} room_id={room} user_id={userId} isHost={isHost} gamemode={gameMode} />
 			) : (
 				<div className="d-flex justify-content-center align-items-center text-light pt-5 mt-5">
+					<p>{JSON.stringify(gameInfos)}</p>
 					<div className="flex-row align-items-center mt-5">
-						<h1>Waiting for an opponent
-							{/* 
-							<h1 className="loading">
-								<span>.</span><span>.</span><span>.</span>
-							</h1> */}
-						</h1>
+						<h1>Waiting for an opponent</h1>
 						<div className="p-5 text-primary" style={{ marginLeft: "41px", marginBottom: "19px", marginTop: "40px" }}>
 							<Spinner animation="border" style={{ width: '15rem', height: '15rem', borderWidth: "45px", borderRightColor: "#ff0000", borderTopRightRadius: "75px", borderTopColor: '#26cc00', borderBottomColor: '#ffd700', animationDuration: "15s" }} />
 						</div>
