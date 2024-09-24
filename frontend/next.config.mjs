667e7da46f681e063e41aecc0fdb205c2ae9dda0
Milan <drownedmilan@gmail.com>
2024-09-24 16:22:05 +0200

    localProps properly passed to Play page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,19 +1,20 @@
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-  export default {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: 'backend',
-          port: '8000',
-        },
-        {
-          protocol: 'http',
-          hostname: 'localhost',
-        },
-      ],
-    },
-  };
+export default {
+	reactStrictMode: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'http',
+				hostname: 'backend',
+				port: '8000',
+			},
+			{
+				protocol: 'http',
+				hostname: 'localhost',
+			},
+		],
+	},
+};
 
 // export default nextConfig;
