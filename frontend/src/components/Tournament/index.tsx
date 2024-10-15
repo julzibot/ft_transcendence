1a502f911d1a4ed747a0b7d88d69160dfafafab6
Milan <drownedmilan@gmail.com>
2024-10-15 20:46:52 +0200

    init socket logic

--- frontend/src/components/Tournament/index.tsx
+++ frontend/src/components/Tournament/index.tsx
@@ -52,14 +52,14 @@ export default function Tournament({ setToastShow, setErrorField, errorField }: 
 
 
 	const handleJoin = async (tournament: Tournament, userId: number, linkToJoin: string) => {
-		if(tournament.isStarted) {
+		if (tournament.isStarted) {
 			setErrorField({ ...errorField, joinError: 'Tournament has already started' })
 			setToastShow(true)
 			return
 		}
 		try {
 			await joinTournament(tournament.id, userId)
-			// router.push(`/tournaments/${linkToJoin}`)
+			router.push(`/tournaments/${linkToJoin}`)
 		}
 		catch (error: any) {
 			setErrorField({ ...errorField, joinError: error.message })
@@ -68,18 +68,18 @@ export default function Tournament({ setToastShow, setErrorField, errorField }: 
 	}
 
 	const submitTournament = async () => {
-		if(tournamentForm.name.trim() === '') {
+		if (tournamentForm.name.trim() === '') {
 			setErrorField({ ...errorField, nameMissing: 'Tournament Name is Required' })
 			return
 		}
-		if(tournamentForm.difficultyLevel === 0) {
+		if (tournamentForm.difficultyLevel === 0) {
 			setErrorField({ ...errorField, difficultyMissing: 'Select a difficulty' })
 			return
 		}
 		const payload = {
 			'name': tournamentForm.name,
 			'maxPlayerNumber': tournamentForm.maxPlayerNumber,
-			'timer': 10,
+			'timer': tournamentForm.timer,
 			'difficultyLevel': tournamentForm.difficultyLevel,
 			'pointsPerGame': tournamentForm.pointsPerGame,
 			'power_ups': tournamentForm.power_ups,
@@ -88,7 +88,7 @@ export default function Tournament({ setToastShow, setErrorField, errorField }: 
 		await CreateTournament(payload)
 		setModalShow(false)
 		setErrorField({ ...errorField, nameMissing: '', difficultyMissing: '' })
-		setTournamentForm({...tournamentForm, name: ''})
+		setTournamentForm({ ...tournamentForm, name: '' })
 		fetchData()
 	}
 
@@ -108,117 +108,117 @@ export default function Tournament({ setToastShow, setErrorField, errorField }: 
 
 	return (
 		<>
-					<div className='d-flex flex-row align-items-center justify-content-between p-4'>
-						<h3 className='mb-0 me-4'>Tournament Lobbies</h3>
-						<Button className="btn btn-outline-light me-md-2" type='button' onClick={() => setModalShow(true)}>Create</Button>
-					</div>
-					<div className='d-flex flex-row align-items-center justify-content-around fw-bold border'>
-						<div className="border-end col-2 d-flex justify-content-center align-items-center">
-							<CustomTooltip text="Tournament Name" position="top">
-								<Alphabet size={24}/>
-							</CustomTooltip>
-						</div >
-						<div className="border-end col-3 d-flex justify-content-center align-items-center" >
-							<CustomTooltip text="Created by" position="top">
-								<PersonFillUp size={15}/>
-							</CustomTooltip>
-						</div>
-						<div className="border-end col-1 d-flex justify-content-center align-items-center" >
-						<CustomTooltip text="Players in Lobby" position="top">
-							<Controller size={15} />
-						</CustomTooltip>
-						</div>
-						<div className="border-end col-1 d-flex justify-content-center align-items-center">
-							<CustomTooltip text="Points Per Game" position="top">
-								<TrophyFill size={15}/>
-							</CustomTooltip>
-						</div>
-						<div className="border-end col-1 d-flex justify-content-center align-items-center">
-							<CustomTooltip text="Difficulty Level" position="top">
-								<Activity size={15}/>
-							</CustomTooltip>
-						</div>
-						<div className="border-end col-1 d-flex justify-content-center align-items-center">
-							<CustomTooltip text="Power Ups" position="top">
-								<LightningFill className="me-1" size={15} />
-							</CustomTooltip>
-						</div>
-						<div className="border-end col-1 d-flex justify-content-center align-items-center">
-							<CustomTooltip text="Duration" position="top">
-								<ClockFill size={15} />
-							</CustomTooltip>
-						</div>
-						<div className="col-1 d-flex justify-content-center align-items-center">
-							<CustomTooltip text="Availability" position="top">
-								<CircleFill size={15} />
-							</CustomTooltip>
-						</div>
-					</div>
-					<div className="mt-2 border scrollbar overflow-y-auto" style={{height: '550px'}}>
-						{
-							tournamentData && tournamentData.length === 0 && <h2 className="text-center mt-5 pt-5">No Tournaments Available</h2>
-						}
-							{
-								tournamentData && tournamentData.map((tournament: TournamentSettingsType, index: number) => {
-									return (
-										<div 
-											type="button" 
-											key={index} 
-											onClick={() => handleJoin(tournament, session.user.id, tournament.linkToJoin)} 
-											className={`${tournamentData.length - 1 === index ? 'border-bottom' : ''} ${index === 0 ?  '' : 'border-top'} tournament-entry d-flex flex-row align-items-center justify-content-around fw-bold fs-5`}
-										>
-											<div className="border-end col-2 d-flex justify-content-center align-items-center text-truncate">
-												{tournament.name}
-											</div>
-											<div className="border-end col-3 d-flex justify-content-center align-items-center text-truncate">
-												<div className="d-flex flex-row align-items-center">
-													<span className="me-2 text-truncate" style={{ maxWidth: 'calc(60%)' }}>{tournament.creator.username}</span>
-														< div className="ms-2 position-relative border border-2 border-dark-subtle rounded-circle" style={{ width: '45px', height: '45px', overflow: 'hidden' }}>
-															<img
-																style={{
-																	objectFit: 'cover',
-																	width: '100%',
-																	height: '100%',
-																	position: 'absolute',
-																	top: '50%',
-																	left: '50%',
-																	transform: 'translate(-50%, -50%)'
-																}}
-																fetchPriority="high"
-																alt="profile picture"
-																src={`${BACKEND_URL}${tournament.creator.image}`}
-															/>
-														</div>
-												</div>
-											</div>
-											<div className="border-end col-1 d-flex justify-content-center align-items-center">
-												<span className="fw-bold">{tournament.numberOfPlayers} / {tournament.maxPlayerNumber}</span>
-											</div>
-											<div className="border-end col-1 d-flex justify-content-center align-items-center">
-												<span className="fw-bold">{tournament.pointsPerGame}</span>
-											</div>
-											<div className="border-end col-1 d-flex justify-content-center align-items-center">
-												<span className="fw-bold">{tournament.difficultyLevel}</span>
-											</div>
-											<div className="border-end col-1 d-flex justify-content-center align-items-center">
-												<span>
-													{
-														tournament.power_ups ? (<Toggle2On size={20} color={'green'} />) : (<Toggle2Off size={20} color={'red'} />)
-													}
-												</span>
-											</div>
-											<div className="col-1 border-end d-flex justify-content-center align-items-center">
-													<span className="ms-1">{tournament.timer}'</span>
-											</div>
-											<div className="col-1 d-flex justify-content-center align-items-center">
-												{
-													tournament.isStarted ? ( <CircleFill size={20} color={'red'}/> ) : ( <CircleFill size={20} color={'green'}/> )
-												}
-											</div>
+			<div className='d-flex flex-row align-items-center justify-content-between p-4'>
+				<h3 className='mb-0 me-4'>Tournament Lobbies</h3>
+				<Button className="btn btn-outline-light me-md-2" type='button' onClick={() => setModalShow(true)}>Create</Button>
+			</div>
+			<div className='d-flex flex-row align-items-center justify-content-around fw-bold border'>
+				<div className="border-end col-2 d-flex justify-content-center align-items-center">
+					<CustomTooltip text="Tournament Name" position="top">
+						<Alphabet size={24} />
+					</CustomTooltip>
+				</div >
+				<div className="border-end col-3 d-flex justify-content-center align-items-center" >
+					<CustomTooltip text="Created by" position="top">
+						<PersonFillUp size={15} />
+					</CustomTooltip>
+				</div>
+				<div className="border-end col-1 d-flex justify-content-center align-items-center" >
+					<CustomTooltip text="Players in Lobby" position="top">
+						<Controller size={15} />
+					</CustomTooltip>
+				</div>
+				<div className="border-end col-1 d-flex justify-content-center align-items-center">
+					<CustomTooltip text="Points Per Game" position="top">
+						<TrophyFill size={15} />
+					</CustomTooltip>
+				</div>
+				<div className="border-end col-1 d-flex justify-content-center align-items-center">
+					<CustomTooltip text="Difficulty Level" position="top">
+						<Activity size={15} />
+					</CustomTooltip>
+				</div>
+				<div className="border-end col-1 d-flex justify-content-center align-items-center">
+					<CustomTooltip text="Power Ups" position="top">
+						<LightningFill className="me-1" size={15} />
+					</CustomTooltip>
+				</div>
+				<div className="border-end col-1 d-flex justify-content-center align-items-center">
+					<CustomTooltip text="Duration" position="top">
+						<ClockFill size={15} />
+					</CustomTooltip>
+				</div>
+				<div className="col-1 d-flex justify-content-center align-items-center">
+					<CustomTooltip text="Availability" position="top">
+						<CircleFill size={15} />
+					</CustomTooltip>
+				</div>
+			</div>
+			<div className="mt-2 border scrollbar overflow-y-auto" style={{ height: '550px' }}>
+				{
+					tournamentData && tournamentData.length === 0 && <h2 className="text-center mt-5 pt-5">No Tournaments Available</h2>
+				}
+				{
+					tournamentData && tournamentData.map((tournament: TournamentSettingsType, index: number) => {
+						return (
+							<div
+								type="button"
+								key={index}
+								onClick={() => handleJoin(tournament, session.user.id, tournament.linkToJoin)}
+								className={`${tournamentData.length - 1 === index ? 'border-bottom' : ''} ${index === 0 ? '' : 'border-top'} tournament-entry d-flex flex-row align-items-center justify-content-around fw-bold fs-5`}
+							>
+								<div className="border-end col-2 d-flex justify-content-center align-items-center text-truncate">
+									{tournament.name}
+								</div>
+								<div className="border-end col-3 d-flex justify-content-center align-items-center text-truncate">
+									<div className="d-flex flex-row align-items-center">
+										<span className="me-2 text-truncate" style={{ maxWidth: 'calc(60%)' }}>{tournament.creator.username}</span>
+										< div className="ms-2 position-relative border border-2 border-dark-subtle rounded-circle" style={{ width: '45px', height: '45px', overflow: 'hidden' }}>
+											<img
+												style={{
+													objectFit: 'cover',
+													width: '100%',
+													height: '100%',
+													position: 'absolute',
+													top: '50%',
+													left: '50%',
+													transform: 'translate(-50%, -50%)'
+												}}
+												fetchPriority="high"
+												alt="profile picture"
+												src={`${BACKEND_URL}${tournament.creator.image}`}
+											/>
 										</div>
-									)
-								})
-							}
+									</div>
+								</div>
+								<div className="border-end col-1 d-flex justify-content-center align-items-center">
+									<span className="fw-bold">{tournament.numberOfPlayers} / {tournament.maxPlayerNumber}</span>
+								</div>
+								<div className="border-end col-1 d-flex justify-content-center align-items-center">
+									<span className="fw-bold">{tournament.pointsPerGame}</span>
+								</div>
+								<div className="border-end col-1 d-flex justify-content-center align-items-center">
+									<span className="fw-bold">{tournament.difficultyLevel}</span>
+								</div>
+								<div className="border-end col-1 d-flex justify-content-center align-items-center">
+									<span>
+										{
+											tournament.power_ups ? (<Toggle2On size={20} color={'green'} />) : (<Toggle2Off size={20} color={'red'} />)
+										}
+									</span>
+								</div>
+								<div className="col-1 border-end d-flex justify-content-center align-items-center">
+									<span className="ms-1">{tournament.timer}'</span>
+								</div>
+								<div className="col-1 d-flex justify-content-center align-items-center">
+									{
+										tournament.isStarted ? (<CircleFill size={20} color={'red'} />) : (<CircleFill size={20} color={'green'} />)
+									}
+								</div>
+							</div>
+						)
+					})
+				}
 
 
 				<Modal
@@ -310,19 +310,19 @@ export default function Tournament({ setToastShow, setErrorField, errorField }: 
 								</select>
 								<label className="text-danger form-label" For="difficultyLevel">{errorField.difficultyMissing}</label>
 							</div>
-								<div className="mb-1 form-check form-switch">
-									<input
-										className="form-check-input"
-										type="checkbox"
-										role="switch"
-										id="flexSwitchCheckChecked"
-										checked={tournamentForm.power_ups}
-										onChange={(e) => setTournamentForm({ ...tournamentForm, power_ups: e.target.checked })}
-									/>
-									<label className="form-check-label">Power ups
-										<LightningFill className="ms-1" size={15} />
-									</label>
-								</div>
+							<div className="mb-1 form-check form-switch">
+								<input
+									className="form-check-input"
+									type="checkbox"
+									role="switch"
+									id="flexSwitchCheckChecked"
+									checked={tournamentForm.power_ups}
+									onChange={(e) => setTournamentForm({ ...tournamentForm, power_ups: e.target.checked })}
+								/>
+								<label className="form-check-label">Power ups
+									<LightningFill className="ms-1" size={15} />
+								</label>
+							</div>
 						</form>
 					</Modal.Body>
 					<Modal.Footer>
--- ws-server/server.js
+++ ws-server/server.js
@@ -8,146 +8,178 @@ const frontendPort = process.env.FRONTEND_PORT || 3000;
 const fetchFinished = new Map();
 const socketRooms = new Map();
 
+const tournamentsArray = [];
+
+// const tournament = {
+// 	tournament_id: 0,
+// 	nb_of_participants: 0,
+// 	players: [],
+// 	rooms: []
+// }
 
 
 const server = createServer();
 
 const io = new Server(server, {
-  cors: {
-    origin: `https://${domain}:${frontendPort}`,
-    methods: ["GET", "POST"],
-    credentials: true
-  }
+	cors: {
+		origin: `https://${domain}:${frontendPort}`,
+		methods: ["GET", "POST"],
+		credentials: true
+	}
 });
 
 io.on("connection", async (socket) => {
-  console.log("User connected through socket: " + socket.id);
-  if (!socketRooms.get(socket.id))
-    socketRooms.set(socket.id, new Set());
+	console.log("User connected through socket: " + socket.id);
 
-  socket.on('join_room', data => {
-    const room = io.sockets.adapter.rooms.get(data.room_id);
+	if (!socketRooms.get(socket.id))
+		socketRooms.set(socket.id, new Set());
 
-    // if (room) {
-    // 	const roomSize = io._nsps.get('/').adapter.rooms.get(data.room_id).size;
-    // 	console.log(`${data.room_id} Two players are already connected`);
-    // 	return;
-    // }
+	socket.on('join_room', data => {
+		const room = io.sockets.adapter.rooms.get(data.room_id);
 
-    if (!room || room.size === 0) {
-      console.log("[HOST] Client [" + data.user_id + "] joining room: " + data.room_id);
-      socket.emit('isHost');
-    }
-    else {
-      console.log("[NOT HOST] Client [" + data.user_id + "] joining room: " + data.room_id);
-      socket.to(data.room_id).emit('player2_id', { player2_id: data.user_id });
-      socket.emit('isNotHost');
-    }
+		// if (room) {
+		// 	const roomSize = io._nsps.get('/').adapter.rooms.get(data.room_id).size;
+		// 	console.log(`${data.room_id} Two players are already connected`);
+		// 	return;
+		// }
 
-    socket.join(data.room_id);
-    socketRooms.get(socket.id).add(data.room_id);
-    if (!fetchFinished.has(data.room_id))
-      fetchFinished.set(data.room_id, 0);
-  });
+		if (!room || room.size === 0) {
+			console.log("[HOST] Client [" + data.user_id + "] joining room: " + data.room_id);
+			socket.emit('isHost');
+		}
+		else {
+			console.log("[NOT HOST] Client [" + data.user_id + "] joining room: " + data.room_id);
+			socket.to(data.room_id).emit('player2_id', { player2_id: data.user_id });
+			socket.emit('isNotHost');
+		}
 
-  socket.on('fetchFinished', (data) => {
-    let currentCount = fetchFinished.get(data.room_id) || 0;
-    if (currentCount > -1)
-      fetchFinished.set(data.room_id, currentCount + 1);
-    if (fetchFinished.get(data.room_id) === 2) {
-      io.in(data.room_id).emit('startGame');
-      fetchFinished.delete(data.room_id);
-    }
-  })
+		socket.join(data.room_id);
+		socketRooms.get(socket.id).add(data.room_id);
+		if (!fetchFinished.has(data.room_id))
+			fetchFinished.set(data.room_id, 0);
+	});
 
-  socket.on('sendGameId', (data) => {
-    socket.to(data.room_id).emit('receiveGameId', { game_id: data.game_id });
-  });
+	socket.on('fetchFinished', (data) => {
+		let currentCount = fetchFinished.get(data.room_id) || 0;
+		if (currentCount > -1)
+			fetchFinished.set(data.room_id, currentCount + 1);
+		if (fetchFinished.get(data.room_id) === 2) {
+			io.in(data.room_id).emit('startGame');
+			fetchFinished.delete(data.room_id);
+		}
 
-  socket.on('sendPlayerInfos', (data) => {
-    socket.to(data.room_id).emit('setPlayerInfos', { p1Name: data.p1Name, p2Name: data.p2Name, p1p: data.p1p, p2p: data.p2p, game_id: data.game_id });
-  })
+	});
 
-  socket.on('sendBallPos', (data) => {
-    socket.to(data.room_id).emit('updateBallPos', { x: data.x, y: data.y, vectx: data.vectx, vecty: data.vecty, speed: data.speed });
-  })
+	// Tournament sockets
+	socket.on('join_tournament', (data) => {
+		// data:
+		// tournament_id,
+		// player_id,
+		// no. of participants
+		if (!tournamentsArray.find(data.tournament_id))
+			tournamentsArray.push({ tournament_id: data.tournament_id, nb_of_participants: data.nb_of_participants, players: [], rooms: [] })
 
-  socket.on('sendPlayer1Pos', data => {
-    socket.to(data.room_id).emit('updatePlayer1Pos', { player1pos: data.player1pos });
-  });
+		socket.join(data.tournament_id);
+		// inform the others that a new player has joined
+		// socket.room(tournament_id).emit('new_player_joined')
+		tournamentsArray.find(data.tournament_id).players.push(data.player_id);
 
-  socket.on('sendPlayer2Pos', data => {
-    socket.to(data.room_id).emit('updatePlayer2Pos', { player2pos: data.player2pos });
-  });
+		socket.emit('join_success', data);
 
-  socket.on('sendBounceGlow', data => {
-    socket.to(data.room_id).emit('startBounceGlow');
-  })
+		if (tournamentsArray.players.size === tournamentsArray.nb_of_participants)
+			io.in(data.tournament_id).emit('tournament_full');
 
-  socket.on('sendWallCollision', data => {
-    socket.to(data.room_id).emit('newWallCollision');
-  })
+	});
 
-  socket.on('sendScore', data => {
-    socket.to(data.room_id).emit('updateScore', { score1: data.score1, score2: data.score2, stopGame: data.stopGame });
-  })
+	// Game match communication
+	socket.on('sendGameId', (data) => {
+		socket.to(data.room_id).emit('receiveGameId', { game_id: data.game_id });
+	});
 
-  socket.on('sendCreatePU', data => {
-    socket.to(data.room_id).emit('updateCreatePU', { pu_id: data.pu_id, powerType: data.type, radius: data.radius, spawnx: data.x, spawny: data.y });
-  })
+	socket.on('sendPlayerInfos', (data) => {
+		socket.to(data.room_id).emit('setPlayerInfos', { p1Name: data.p1Name, p2Name: data.p2Name, p1p: data.p1p, p2p: data.p2p, game_id: data.game_id });
+	})
 
-  socket.on('sendCollectPU', data => {
-    socket.to(data.room_id).emit('updateCollectPU', { player_id: data.player_id, powerType: data.power_id });
-  })
+	socket.on('sendBallPos', (data) => {
+		socket.to(data.room_id).emit('updateBallPos', { x: data.x, y: data.y, vectx: data.vectx, vecty: data.vecty, speed: data.speed });
+	})
 
-  socket.on('sendActivatePU1', data => {
-    socket.to(data.room_id).emit('updateActivatePU1', { powerType: data.powerType });
-  })
+	socket.on('sendPlayer1Pos', data => {
+		socket.to(data.room_id).emit('updatePlayer1Pos', { player1pos: data.player1pos });
+	});
 
-  socket.on('sendActivatePU2', data => {
-    socket.to(data.room_id).emit('updateActivatePU2', { powerType: data.powerType });
-  })
+	socket.on('sendPlayer2Pos', data => {
+		socket.to(data.room_id).emit('updatePlayer2Pos', { player2pos: data.player2pos });
+	});
 
-  socket.on('sendInvert', data => {
-    socket.to(data.room_id).emit('updateInvert');
-  })
+	socket.on('sendBounceGlow', data => {
+		socket.to(data.room_id).emit('startBounceGlow');
+	})
 
-  socket.on('sendInvisiball', data => {
-    socket.to(data.room_id).emit('updateInvisiball', { id: data.player_id });
-  })
+	socket.on('sendWallCollision', data => {
+		socket.to(data.room_id).emit('newWallCollision');
+	})
 
-  socket.on('sendDeactivatePU', data => {
-    socket.to(data.room_id).emit('updateDeactivatePU', { player_id: data.player_id, type: data.type });
-  })
+	socket.on('sendScore', data => {
+		socket.to(data.room_id).emit('updateScore', { score1: data.score1, score2: data.score2, stopGame: data.stopGame });
+	})
 
-  socket.on('sendDeletePU', data => {
-    socket.to(data.room_id).emit('updateDeletePU', { pu_id: data.pu_id });
-  })
-  socket.on('Enter_Tournaments_lobby', data => {
-    console.log(`${data.userId} is in the main lobby`)
-  })
+	socket.on('sendCreatePU', data => {
+		socket.to(data.room_id).emit('updateCreatePU', { pu_id: data.pu_id, powerType: data.type, radius: data.radius, spawnx: data.x, spawny: data.y });
+	})
 
-  socket.on('updateTournament', data => {
-    socket.emit('updateTournament', data)
-  })
+	socket.on('sendCollectPU', data => {
+		socket.to(data.room_id).emit('updateCollectPU', { player_id: data.player_id, powerType: data.power_id });
+	})
 
-  socket.on('Leave_Tournaments_lobby', data => {
-    console.log(`${data.userId} left main lobby`)
-  })
+	socket.on('sendActivatePU1', data => {
+		socket.to(data.room_id).emit('updateActivatePU1', { powerType: data.powerType });
+	})
 
-  socket.on('disconnect', () => {
-    console.log(socket.id + " disconnected");
-    socketRooms.get(socket.id).forEach(room_id => {
-      console.log(`[Server] Sending Disconnection Event -> [room] ${room_id}`)
-      socket.to(room_id).emit('playerDisconnected');
-    });
-    socketRooms.delete(socket.id);
-  });
+	socket.on('sendActivatePU2', data => {
+		socket.to(data.room_id).emit('updateActivatePU2', { powerType: data.powerType });
+	})
+
+	socket.on('sendInvert', data => {
+		socket.to(data.room_id).emit('updateInvert');
+	})
+
+	socket.on('sendInvisiball', data => {
+		socket.to(data.room_id).emit('updateInvisiball', { id: data.player_id });
+	})
+
+	socket.on('sendDeactivatePU', data => {
+		socket.to(data.room_id).emit('updateDeactivatePU', { player_id: data.player_id, type: data.type });
+	})
+
+	socket.on('sendDeletePU', data => {
+		socket.to(data.room_id).emit('updateDeletePU', { pu_id: data.pu_id });
+	})
+	socket.on('Enter_Tournaments_lobby', data => {
+		console.log(`${data.userId} is in the main lobby`)
+	})
+
+	socket.on('updateTournament', data => {
+		socket.emit('updateTournament', data)
+	})
+
+	socket.on('Leave_Tournaments_lobby', data => {
+		console.log(`${data.userId} left main lobby`)
+	})
+
+	socket.on('disconnect', () => {
+		console.log(socket.id + " disconnected");
+		socketRooms.get(socket.id).forEach(room_id => {
+			console.log(`[Server] Sending Disconnection Event -> [room] ${room_id}`)
+			socket.to(room_id).emit('playerDisconnected');
+		});
+		socketRooms.delete(socket.id);
+	});
 
 
 });
 
 server.listen(port, () => {
-  console.log(`Socket server is now listening on https://${domain}:` + port);
+	console.log(`Socket server is now listening on https://${domain}:` + port);
 });
 
